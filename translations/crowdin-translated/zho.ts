3585d2cede5f13b6f6b97f721e6bc135e015e8f3
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-20 23:11:20 +0800

    New translations tagForm.ts (Chinese Simplified)

--- translations/crowdin-translated/zho.ts
+++ translations/crowdin-translated/zho.ts
@@ -1,6 +1,5 @@
-const seriesReader = {
-  LABEL_LOADING_CHAPTER: 'Loading chapter...',
-  LABEL_NO_CHAPTER_SELECTED: 'No chapter selected',
-  LABEL_NO_PERMISSION_TO_VIEW: 'No permission to view'
+const tagForm = {
+  LABEL_TAG_TEXTFIELD: 'Tags(optional - max 5)',
+  LABEL_TAG_PLACEHOLDER: 'Tags'
 };
-export default seriesReader;
+export default tagForm;
