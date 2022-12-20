b67de39a76460fd89ddd2bf0cc5146366120329d
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-20 23:11:51 +0800

    New translations account.ts (Cebuano)

--- translations/crowdin-translated/ceb.ts
+++ translations/crowdin-translated/ceb.ts
@@ -1,17 +1,11 @@
 import topNav from './commons/topNav';
 import common from './commons/common';
 import authModal from './commons/authModal';
-const home = { ...topNav,
+const account = { ...topNav,
   ...common,
   ...authModal,
-  LABEL_ABOUT_US_HEADER_5: 'ABOUT US',
-  LABEL_ABOUT_US_BODY_5_1: "Bisag Unsa.",
-  LABEL_ABOUT_US_BODY_5_2: 'We also believe great comics can come from anywhere.',
-  LABEL_ABOUT_US_BODY_5_3: 'We are a group of comic fans determined to build tools',
-  LABEL_ABOUT_US_BODY_5_4: 'to help comic creators connect their stories to as many people as possible.',
-  LABEL_ABOUT_US_HEADER_6: "WHAT'S YOUR STORY?",
-  LABEL_SIGN_UP: 'Sign up',
-  LABEL_CREATE_SERIES: 'Create series',
-  LABEL_MY_PROFILE: 'My profile'
+  LABEL_SIGN_IN: 'Sign in',
+  LABEL_SIGN_OUT: 'Sign out',
+  LABEL_SETTINGS: 'Settings'
 };
-export default home;
+export default account;
