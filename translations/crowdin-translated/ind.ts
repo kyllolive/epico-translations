ceaa08a90ccb5e05801064e13672fbd281792dbe
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-20 23:13:32 +0800

    New translations profile.ts (Indonesian)

--- translations/crowdin-translated/ind.ts
+++ translations/crowdin-translated/ind.ts
@@ -1,36 +1,25 @@
-import home from './home';
-import about from './about';
-import genre from './genre';
-import settings from './settings';
-import account from './account';
-import search from './search';
-import viewChapters from './viewChapters';
-import viewChapter from './viewChapter';
-import consoleSeriesList from './consoleSeriesList';
-import consoleCreateSeries from './consoleCreateSeries';
-import consoleEditSeries from './consoleEditSeries';
-import consoleChapterList from './consoleChapterList';
-import consoleCreateChapter from './consoleCreateChapter';
-import consoleEditChapter from './consoleEditChapter';
-import profile from './profile';
-import translate from './translate';
-import dashboard from './dashboard';
-export default {
-  home,
-  about,
-  genre,
-  settings,
-  account,
-  search,
-  viewChapters,
-  viewChapter,
-  consoleSeriesList,
-  consoleCreateSeries,
-  consoleEditSeries,
-  consoleChapterList,
-  consoleCreateChapter,
-  consoleEditChapter,
-  profile,
-  translate,
-  dashboard
+import genres from './commons/genres';
+import topNav from './commons/topNav';
+import common from './commons/common';
+import authModal from './commons/authModal';
+import donationBox from './commons/donationBox';
+const profile = { ...donationBox,
+  ...genres,
+  ...topNav,
+  ...common,
+  ...authModal,
+  NEW_COVER_ERROR_LABEL: 'Oops! We were unable to save your new cover',
+  LABEL_SAVE: 'Save',
+  LABEL_CANCEL: 'Cancel',
+  LABEL_SUCCESS_UPDATE_PROFILE: 'We were successful updating your profile',
+  LABEL_UNABLE_SAVE_CHANGES: 'Uh oh! We were unable to save your changes',
+  LABEL_ADD_ANOTHER: 'Add Another',
+  LABEL_EDIT_PROFILE: 'Edit Profile',
+  LABEL_JOINED_DATE: 'Joined date',
+  LABEL_UPDATE_PROFILE_INFORMATION: 'Quick update profile information',
+  LABEL_CHANGE_PROFILE_PICTURE: 'Change profile picture',
+  LABEL_ERROR_NEW_PROFILE: 'Oops! We were unable to save your new profile',
+  LABEL_ERROR_NEW_COVER: 'Oops! We were unable to save your new cover',
+  LABEL_ERROR_UPLOAD_COVER: 'Oops! We were unable to upload your cover'
 };
+export default profile;
