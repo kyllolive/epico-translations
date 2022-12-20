6d8227ec284336c95ca27523d58cadf6cb444be4
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-20 23:10:24 +0800

    New translations genres.ts (Chinese Simplified)

--- translations/crowdin-translated/zho.ts
+++ translations/crowdin-translated/zho.ts
@@ -1,5 +1,37 @@
-const followButton = {
-  LABEL_FOLLOW: 'Follow',
-  LABEL_FOLLOWING: 'Following'
+const genres = {
+  GENRE_ALL: 'All',
+  GENRE_ACTION: 'Action',
+  GENRE_ADVENTURE: 'Adventure',
+  GENRE_COMEDY: 'Comedy',
+  GENRE_CYBERPUNK: 'Cyberpunk',
+  GENRE_DEMONS: 'Demons',
+  GENRE_DRAMA: 'Drama',
+  GENRE_FANTASY: 'Fantasy',
+  GENRE_GAME: 'Game',
+  GENRE_HISTORICAL: 'Historical',
+  GENRE_HORROR: 'Horror',
+  GENRE_KIDS: 'Kids',
+  GENRE_LGBTQ: 'LGBTQ+',
+  GENRE_MAGIC: 'Magic',
+  GENRE_MATURE: 'Mature',
+  GENRE_MECHA: 'Mecha',
+  GENRE_MILITARY: 'Military',
+  GENRE_MUSIC: 'Music',
+  GENRE_MYSTERY: 'Mystery',
+  GENRE_PARODY: 'Parody',
+  GENRE_POLICE: 'Police',
+  GENRE_POST_APOCALYPTIC: 'Post-Apocalyptic',
+  GENRE_PSYCHOLOGICAL: 'Psychological',
+  GENRE_REVERSE_HAREM: 'Reverse Harem',
+  GENRE_ROMANCE: 'Romance',
+  GENRE_SCHOOL: 'School',
+  GENRE_SCI_FI: 'Sci-Fi',
+  GENRE_SLICE_OF_LIFE: 'Slice of Life',
+  GENRE_SPACE: 'Space',
+  GENRE_SPORTS: 'Sports',
+  GENRE_SUPER_POWER: 'Super Power',
+  GENRE_SUPERNATURAL: 'Supernatural',
+  GENRE_TRAGEDY: 'Tragedy',
+  GENRE_VAMPIRE: 'Vampire'
 };
-export default followButton;
+export default genres;
