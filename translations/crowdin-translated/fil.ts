7298068ea70741083c317e3a5b157cbd4ae8321e
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-20 23:11:05 +0800

    New translations seriesFooter.ts (Filipino)

--- translations/crowdin-translated/fil.ts
+++ translations/crowdin-translated/fil.ts
@@ -1,15 +1,16 @@
-const seriesDrawer = {
+const seriesFooter = {
   LABEL_INFORMATION_TAB_NEW_SERIES_MSG: 'NEW CHAPTER EVERY MONDAY, FRIDAY',
-  LABEL_INFORMATION: 'Information',
-  LABEL_LANGUAGE: 'Language',
-  LABEL_READ_CHAPTER_1: 'Read: Chapter 1',
-  LABEL_CREATORS: 'Creators',
-  LABEL_PUBLISHER: 'Publisher',
-  LABEL_ARTIST: 'Artist',
-  LABEL_AUTHOR: 'Author',
-  LABEL_SEE_MORE: 'See more',
+  LABEL_PREVIOUS: 'Previous',
+  LABEL_NEXT: 'Next',
+  LABEL_COMMENTS: 'Comments',
+  LABEL_POPULAR: 'Popular',
+  LABEL_NEWEST: 'Newest',
+  LABEL_REPLY: 'Reply',
+  LABEL_SHOW_MORE: 'Show more',
   LABEL_PUBLISHER_OTHER_WORK: "Publisher's other works",
   LABEL_SERIES_YOU_MIGHT_LIKE: 'Series you might like',
-  LABEL_NOT_PUBLISHED_CHAPTER: 'Not published'
+  LABEL_CREATOR: 'Creator',
+  LABEL_ADD_A_COMMENT: 'Add a comment',
+  LABEL_SEE_ALL: 'See all'
 };
-export default seriesDrawer;
+export default seriesFooter;
