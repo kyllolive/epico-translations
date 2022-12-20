fa3938844199ac9a22c1d9c8f5225315bea9a03b
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-20 23:11:11 +0800

    New translations seriesReader.ts (Chinese Simplified)

--- translations/crowdin-translated/zho.ts
+++ translations/crowdin-translated/zho.ts
@@ -1,16 +1,6 @@
-const seriesFooter = {
-  LABEL_INFORMATION_TAB_NEW_SERIES_MSG: 'NEW CHAPTER EVERY MONDAY, FRIDAY',
-  LABEL_PREVIOUS: 'Previous',
-  LABEL_NEXT: 'Next',
-  LABEL_COMMENTS: 'Comments',
-  LABEL_POPULAR: 'Popular',
-  LABEL_NEWEST: 'Newest',
-  LABEL_REPLY: 'Reply',
-  LABEL_SHOW_MORE: 'Show more',
-  LABEL_PUBLISHER_OTHER_WORK: "Publisher's other works",
-  LABEL_SERIES_YOU_MIGHT_LIKE: 'Series you might like',
-  LABEL_CREATOR: 'Creator',
-  LABEL_ADD_A_COMMENT: 'Add a comment',
-  LABEL_SEE_ALL: 'See all'
+const seriesReader = {
+  LABEL_LOADING_CHAPTER: 'Loading chapter...',
+  LABEL_NO_CHAPTER_SELECTED: 'No chapter selected',
+  LABEL_NO_PERMISSION_TO_VIEW: 'No permission to view'
 };
-export default seriesFooter;
+export default seriesReader;
