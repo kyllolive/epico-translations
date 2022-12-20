58e85d1a8a69124ffe699ec2c844ad27d8ae04e4
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-20 23:11:58 +0800

    New translations consoleChapterList.ts (Indonesian)

--- translations/crowdin-translated/ind.ts
+++ translations/crowdin-translated/ind.ts
@@ -1,11 +1,26 @@
 import topNav from './commons/topNav';
 import common from './commons/common';
-import authModal from './commons/authModal';
-const account = { ...topNav,
+import deleteDialog from './commons/deleteDialog';
+const consoleChapterList = { ...topNav,
   ...common,
-  ...authModal,
-  LABEL_SIGN_IN: 'Sign in',
-  LABEL_SIGN_OUT: 'Sign out',
-  LABEL_SETTINGS: 'Settings'
+  ...deleteDialog,
+  LABEL_COLUMN_ID: 'ID',
+  LABEL_ADD_NEW_CHAPTER: 'Add New Chapter',
+  LABEL_COLUMN_TITLE: 'Title',
+  LABEL_COLUMN_LANGUAGE: 'Language',
+  LABEL_COLUMN_STATUS: 'Status',
+  LABEL_COLUMN_CHAPTERS: 'Chapters',
+  LABEL_COLUMN_PUBLISHED: 'Published',
+  LABEL_COLUMN_DESCRIPTION: 'Description',
+  LABEL_COLUMN_PAGES: 'Pages',
+  LABEL_COLUMN_CREATED_DATE: 'Created Date',
+  LABEL_NO_CHAPTERS: 'No Chapters',
+  LABEL_NO_SERIES: 'No Series',
+  LABEL_TRANSLATION: 'Translation',
+  LABEL_EDIT: 'Edit',
+  LABEL_DELETE: 'Delete',
+  LABEL_ROWS_PER_PAGE: 'Rows per page:',
+  LABEL_PRIVATE_VIEW: 'Private View',
+  LABEL_SHARE_ACTION_MENU: 'Share/Collaborators'
 };
-export default account;
+export default consoleChapterList;
