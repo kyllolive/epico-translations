ece928a576f680d61d6c6f0ce3a92dbf27cb82bb
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:18:26 +0800

    New translations donationBox.ts (Chinese Simplified)

--- crowdin-translate/zh/commons/donationBox.ts
+++ crowdin-translate/zh/commons/donationBox.ts
@@ -1,9 +1,8 @@
 const donationBox = {
-  LABEL_DONATE_FOR: '捐赠给',
-  LABEL_DONATE_BACK: '后退',
-  LABEL_DONATE_MINIMUM: '最低捐款为',
-  LABEL_DONATE: '捐',
-  LABEL_EPICO_DESCRIPTION: 'Epico 不收取任何费用！',
+  LABEL_DONATE_FOR: 'Donate for',
+  LABEL_DONATE_BACK: 'Back',
+  LABEL_DONATE_MINIMUM: 'Minimum donation is',
+  LABEL_DONATE: 'Donate',
+  LABEL_EPICO_DESCRIPTION: 'Epico doesn’t take a fee!'
 };
-
 export default donationBox;
