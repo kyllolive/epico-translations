ec28ba697c883d25c902608dd1837bc7da744cee
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:21:13 +0800

    New translations profile.ts (Cebuano)

--- crowdin-translate/ceb/profile.ts
+++ crowdin-translate/ceb/profile.ts
@@ -3,28 +3,23 @@ import topNav from './commons/topNav';
 import common from './commons/common';
 import authModal from './commons/authModal';
 import donationBox from './commons/donationBox';
-
-const profile = {
-  ...donationBox,
+const profile = { ...donationBox,
   ...genres,
   ...topNav,
   ...common,
   ...authModal,
-  NEW_COVER_ERROR_LABEL: 'Oops! Wala namo ma-save ang imong bag-ong hapin',
-  LABEL_SAVE: 'Tipigi',
-  LABEL_CANCEL: 'Pagkanselar',
-  LABEL_SUCCESS_UPDATE_PROFILE:
-    'Nagmalampuson kami sa pag-update sa imong profile',
-  LABEL_UNABLE_SAVE_CHANGES: 'Uh oh! Wala namo ma-save ang imong mga kausaban',
-  LABEL_ADD_ANOTHER: 'Pagdugang og Lain',
-  LABEL_EDIT_PROFILE: 'Usba ang Profile',
-  LABEL_JOINED_DATE: 'Petsa sa pag-apil',
-  LABEL_UPDATE_PROFILE_INFORMATION:
-    'Dali nga pag-update sa impormasyon sa profile',
-  LABEL_CHANGE_PROFILE_PICTURE: 'Usba ang hulagway sa profile',
-  LABEL_ERROR_NEW_PROFILE: 'Oops! Wala namo ma-save ang imong bag-ong profile',
-  LABEL_ERROR_NEW_COVER: 'Oops! Wala namo ma-save ang imong bag-ong hapin',
-  LABEL_ERROR_UPLOAD_COVER: 'Oops! Wala namo ma-upload ang imong hapin',
+  NEW_COVER_ERROR_LABEL: 'Oops! We were unable to save your new cover',
+  LABEL_SAVE: 'Save',
+  LABEL_CANCEL: 'Cancel',
+  LABEL_SUCCESS_UPDATE_PROFILE: 'We were successful updating your profile',
+  LABEL_UNABLE_SAVE_CHANGES: 'Uh oh! We were unable to save your changes',
+  LABEL_ADD_ANOTHER: 'Add Another',
+  LABEL_EDIT_PROFILE: 'Edit Profile',
+  LABEL_JOINED_DATE: 'Joined date',
+  LABEL_UPDATE_PROFILE_INFORMATION: 'Quick update profile information',
+  LABEL_CHANGE_PROFILE_PICTURE: 'Change profile picture',
+  LABEL_ERROR_NEW_PROFILE: 'Oops! We were unable to save your new profile',
+  LABEL_ERROR_NEW_COVER: 'Oops! We were unable to save your new cover',
+  LABEL_ERROR_UPLOAD_COVER: 'Oops! We were unable to upload your cover'
 };
-
 export default profile;
