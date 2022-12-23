f3706af44cfbd7bbc7a0dbef152de04152109ef4
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:20:29 +0800

    New translations consoleSeriesList.ts (Indonesian)

--- crowdin-translate/id/consoleSeriesList.ts
+++ crowdin-translate/id/consoleSeriesList.ts
@@ -2,32 +2,29 @@ import genres from './commons/genres';
 import topNav from './commons/topNav';
 import common from './commons/common';
 import deleteDialog from './commons/deleteDialog';
-
-const consoleSeriesList = {
-  ...genres,
+const consoleSeriesList = { ...genres,
   ...topNav,
   ...common,
   ...deleteDialog,
-  LABEL_COLUMN_ID: 'PENGENAL',
-  LABEL_ADD_NEW_SERIES: 'Tambahkan Seri Baru',
-  LABEL_COLUMN_TITLE: 'Judul',
-  LABEL_COLUMN_LANGUAGE: 'Bahasa',
+  LABEL_COLUMN_ID: 'ID',
+  LABEL_ADD_NEW_SERIES: 'Add New Series',
+  LABEL_COLUMN_TITLE: 'Title',
+  LABEL_COLUMN_LANGUAGE: 'Language',
   LABEL_COLUMN_STATUS: 'Status',
-  LABEL_COLUMN_CHAPTERS: 'bab',
-  LABEL_COLUMN_PUBLISHED: 'Diterbitkan',
-  LABEL_COLUMN_DESCRIPTION: 'Keterangan',
-  LABEL_COLUMN_PAGES: 'halaman',
-  LABEL_COLUMN_CREATED_DATE: 'Tanggal Dibuat',
-  LABEL_NO_CHAPTERS: 'Tidak ada Bab',
-  LABEL_NO_SERIES: 'Tidak ada Seri',
-  LABEL_TRANSLATION: 'Terjemahan',
-  LABEL_EDIT: 'Sunting',
-  LABEL_DELETE: 'Menghapus',
-  LABEL_ROWS_PER_PAGE: 'Baris per halaman:',
-  LABEL_PRIVATE_VIEW: 'Pemandangan Pribadi',
-  LABEL_SHARE_ACTION_MENU: 'Bagikan/Kolaborator',
-  LABEL_SHARED_SERIES: 'Seri Bersama',
-  LABEL_COLUMN_ROLE: 'Peran',
+  LABEL_COLUMN_CHAPTERS: 'Chapters',
+  LABEL_COLUMN_PUBLISHED: 'Published',
+  LABEL_COLUMN_DESCRIPTION: 'Description',
+  LABEL_COLUMN_PAGES: 'Pages',
+  LABEL_COLUMN_CREATED_DATE: 'Created Date',
+  LABEL_NO_CHAPTERS: 'No Chapters',
+  LABEL_NO_SERIES: 'No Series',
+  LABEL_TRANSLATION: 'Translation',
+  LABEL_EDIT: 'Edit',
+  LABEL_DELETE: 'Delete',
+  LABEL_ROWS_PER_PAGE: 'Rows per page:',
+  LABEL_PRIVATE_VIEW: 'Private View',
+  LABEL_SHARE_ACTION_MENU: 'Share/Collaborators',
+  LABEL_SHARED_SERIES: 'Shared Series',
+  LABEL_COLUMN_ROLE: 'Role'
 };
-
 export default consoleSeriesList;
