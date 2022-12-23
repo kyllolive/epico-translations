aa41323a5182c47e2cb8750cc3ca400b5e541e77
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:22:16 +0800

    New translations viewChapter.ts (Japanese)

--- crowdin-translate/ja/viewChapter.ts
+++ crowdin-translate/ja/viewChapter.ts
@@ -5,18 +5,15 @@ import seriesReader from './commons/seriesReader';
 import seriesFooter from './commons/seriesFooter';
 import authModal from './commons/authModal';
 import followButton from './commons/followButton';
-
-const viewChapter = {
-  ...genres,
+const viewChapter = { ...genres,
   ...common,
   ...seriesDrawer,
   ...seriesReader,
   ...seriesFooter,
   ...authModal,
   ...followButton,
-  LABEL_WARNING_MESSAGE: `このシリーズには成人向けのテーマとシチュエーションが含まれており、推奨されます
-  成熟した視聴者向け。視聴者の裁量が推奨されます。表示に進む
-  コンテンツ？`,
+  LABEL_WARNING_MESSAGE: `This Series contains adult themes and situations and is recommended
+  for mature audiences. Viewer's discretions is advised. Proceed to view
+  content?`
 };
-
 export default viewChapter;
