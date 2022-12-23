c6ed19ea883e878c622be2ed530565c7cff9eccd
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:20:26 +0800

    New translations consoleSeriesList.ts (Spanish)

--- crowdin-translate/es/consoleSeriesList.ts
+++ crowdin-translate/es/consoleSeriesList.ts
@@ -2,32 +2,29 @@ import genres from './commons/genres';
 import topNav from './commons/topNav';
 import common from './commons/common';
 import deleteDialog from './commons/deleteDialog';
-
-const consoleSeriesList = {
-  ...genres,
+const consoleSeriesList = { ...genres,
   ...topNav,
   ...common,
   ...deleteDialog,
-  LABEL_COLUMN_ID: 'IDENTIFICACIÓN',
-  LABEL_ADD_NEW_SERIES: 'Agregar nueva serie',
-  LABEL_COLUMN_TITLE: 'Título',
-  LABEL_COLUMN_LANGUAGE: 'Idioma',
-  LABEL_COLUMN_STATUS: 'Estado',
-  LABEL_COLUMN_CHAPTERS: 'Capítulos',
-  LABEL_COLUMN_PUBLISHED: 'Publicado',
-  LABEL_COLUMN_DESCRIPTION: 'Descripción',
-  LABEL_COLUMN_PAGES: 'Paginas',
-  LABEL_COLUMN_CREATED_DATE: 'Fecha de creación',
-  LABEL_NO_CHAPTERS: 'Sin capítulos',
-  LABEL_NO_SERIES: 'Sin serie',
-  LABEL_TRANSLATION: 'Traducción',
-  LABEL_EDIT: 'Editar',
-  LABEL_DELETE: 'Borrar',
-  LABEL_ROWS_PER_PAGE: 'Filas por página:',
-  LABEL_PRIVATE_VIEW: 'Vista privada',
-  LABEL_SHARE_ACTION_MENU: 'Compartir/Colaboradores',
-  LABEL_SHARED_SERIES: 'Serie compartida',
-  LABEL_COLUMN_ROLE: 'Role',
+  LABEL_COLUMN_ID: 'ID',
+  LABEL_ADD_NEW_SERIES: 'Add New Series',
+  LABEL_COLUMN_TITLE: 'Title',
+  LABEL_COLUMN_LANGUAGE: 'Language',
+  LABEL_COLUMN_STATUS: 'Status',
+  LABEL_COLUMN_CHAPTERS: 'Chapters',
+  LABEL_COLUMN_PUBLISHED: 'Published',
+  LABEL_COLUMN_DESCRIPTION: 'Description',
+  LABEL_COLUMN_PAGES: 'Pages',
+  LABEL_COLUMN_CREATED_DATE: 'Created Date',
+  LABEL_NO_CHAPTERS: 'No Chapters',
+  LABEL_NO_SERIES: 'No Series',
+  LABEL_TRANSLATION: 'Translation',
+  LABEL_EDIT: 'Edit',
+  LABEL_DELETE: 'Delete',
+  LABEL_ROWS_PER_PAGE: 'Rows per page:',
+  LABEL_PRIVATE_VIEW: 'Private View',
+  LABEL_SHARE_ACTION_MENU: 'Share/Collaborators',
+  LABEL_SHARED_SERIES: 'Shared Series',
+  LABEL_COLUMN_ROLE: 'Role'
 };
-
 export default consoleSeriesList;
