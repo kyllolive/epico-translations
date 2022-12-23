4b998f8eeb94d5b01c65630e85ece844acf30a5b
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:31:18 +0800

    New translations imageUploader.ts (Hindi)

---
+++ crowdin-translate/hi/crowdin-translate/ja/commons/imageUploader.ts
@@ -0,0 +1,24 @@
+const imageUploader = {
+  LABEL_UPLOADING_WRONG_RESO: 'おっとっと！間違った解像度でアップロードしているようです',
+  LABEL_UPLOADING_MULTI_FILES: 'おっとっと！複数のファイルをアップロードしているようです',
+  LABEL_UPLOADING_NOT_SUPPORTED_FILES: 'おっとっと！サポートされていないファイル形式をアップロードしているようです',
+  LABEL_UPLOADING_MAX_FILES: 'おっとっと！最大ファイル サイズを超えてアップロードしているようです',
+  LABEL_UPLOADING_MIN_FILES: 'おっとっと！最小ファイル サイズよりも小さいサイズでアップロードしているようです',
+  LABEL_UPLOADING_CORRUPTED_FILES: 'おっとっと！破損したファイルをアップロードしているようです',
+  LABEL_UPLOADING_REACH_MAX_SIZE: 'おっとっと！合計最大ファイル サイズに達したようです',
+  LABEL_UPLOADING_REACH_MAX_FILE_COUNT: 'おっとっと！最大数に達したようです。許可されるファイルの数',
+  LABEL_CORRUPTED: '破損したファイル オブジェクト',
+  LABEL_FILE: 'ファイル',
+  LABEL_SIZE: 'サイズ',
+  LABEL_TOTAL: '合計',
+  LABEL_NAME: '名前',
+  LABEL_TYPE: 'タイプ',
+  LABEL_PAGE: 'ページ',
+  LABEL_PAGES: 'ページ',
+  LABEL_DROP_IMAGE_HERE: 'ここに画像をドロップしてください',
+  LABEL_OPTIMAL_PAGES_DIMENSION: 'すべてのデバイスで最適な結果を得るには、各ページが 16000 x 16000 ピクセルを超えないようにする必要があります',
+  LABEL_DROP_IT_HERE: 'ここにドロップ！',
+  LABEL_LOADING_IMG: '画像を読み込んでいます...',
+  LABEL_MAX: '最大'
+};
+export default imageUploader;
