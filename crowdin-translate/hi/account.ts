a862d56c7eed15f09a613d6b0ce05fe9d496bdb0
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:18:21 +0800

    New translations account.ts (Hindi)

--- crowdin-translate/hi/account.ts
+++ crowdin-translate/hi/account.ts
@@ -1,14 +1,11 @@
 import topNav from './commons/topNav';
 import common from './commons/common';
 import authModal from './commons/authModal';
-
-const account = {
-  ...topNav,
+const account = { ...topNav,
   ...common,
   ...authModal,
-  LABEL_SIGN_IN: 'साइन इन करें',
-  LABEL_SIGN_OUT: 'साइन आउट',
-  LABEL_SETTINGS: 'समायोजन',
+  LABEL_SIGN_IN: 'Sign in',
+  LABEL_SIGN_OUT: 'Sign out',
+  LABEL_SETTINGS: 'Settings'
 };
-
 export default account;
