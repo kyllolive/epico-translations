9c79015871aa5f4d043213f58ac7cadefb9f2216
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:18:59 +0800

    New translations landingPage.ts (Indonesian)

--- crowdin-translate/id/commons/landingPage.ts
+++ crowdin-translate/id/commons/landingPage.ts
@@ -6,34 +6,30 @@ const landingPage = {
   LABEL_WORKS_1: 'Creator uploads original comic series',
   LABEL_WORKS_2: 'Translator/Cleaner discovers comic series',
   LABEL_WORKS_3: 'Cleaner removes the text from artwork',
-  LABEL_WORKS_4:
-    'Penerjemah membuat terjemahan menggunakan alat pengaturan huruf dalam browser',
-  LABEL_WORKS_5:
-    'Kreator menyetujui hasil edit dan memiliki hak publikasi final',
-  LABEL_WORKS_6: 'Pembaca menemukan dan menikmati versi terjemahan',
-  LABEL_BENEFITS: 'Siapa yang diuntungkan?',
-  LABEL_BENEFITOR_1: 'Pencipta',
-  LABEL_BENEFITOR_1_DESCRIPTION: `Tingkatkan jangkauan/pemirsa/penggemar
-  Cara termudah untuk menerjemahkan komik Anda
-  Terima bantuan tanpa pamrih
-  Hasilkan setiap kali komik Anda dibaca
-  Hemat waktu dan uang untuk terjemahan.`,
-  LABEL_BENEFITOR_2: 'Pembaca',
-  LABEL_BENEFITOR_2_DESCRIPTION: `Akses tak terbatas ke semua komik, novel grafis, manhwa, dan manga dari seluruh dunia
-  Dukung pembuat konten melalui donasi bulanan. Kami membagi semua donasi di seluruh judul yang Anda baca.`,
-  LABEL_BENEFITOR_3: 'Pemindai',
-  LABEL_BENEFITOR_3_DESCRIPTION: `Dukung pembuat konten dengan membantu mereka menjangkau pasar yang lebih besar
-  Dapatkan akses ke semua komik secara gratis
-  Bekerja dengan pembuat konten dalam lingkungan hukum
-  Dialog/manuskrip yang dapat dicari
-  Glosarium bawaan untuk mengganti tim`,
-  LABEL_FEATURES: 'Fitur',
-  LABEL_FEATURES_TITLE_1: 'Terjemahan Komik CrowdSourced',
-  LABEL_FEATURES_TITLE_1_SUB_1: 'Akses ke penerjemah komik di seluruh dunia',
-  LABEL_FEATURES_TITLE_1_SUB_1_DESCRIPTION:
-    'Access to thousands of passionate multilingual comics translators worldwide',
-  LABEL_FEATURES_TITLE_1_SUB_2:
-    'Effortless collaboration in a single workspace',
+  LABEL_WORKS_4: 'Translator creates the translations using an inbrowser typesetting tool',
+  LABEL_WORKS_5: 'Creator approves the edits and have final publish rights',
+  LABEL_WORKS_6: 'Reader discovers and enjoys the translated version',
+  LABEL_BENEFITS: 'Who it benefits?',
+  LABEL_BENEFITOR_1: 'Creators',
+  LABEL_BENEFITOR_1_DESCRIPTION: `Increase reach/audience/fans
+  Easiest way to get your comics translated
+  Receive help with no strings attached
+  Earn every time your comics are read
+  Save time and money on translations.`,
+  LABEL_BENEFITOR_2: 'Readers',
+  LABEL_BENEFITOR_2_DESCRIPTION: `Unlimited access to all comics, graphic novels, manhwa and manga from all around the world
+  Support creators through monthly donations. We split all donations across titles you read.`,
+  LABEL_BENEFITOR_3: 'Scanlators',
+  LABEL_BENEFITOR_3_DESCRIPTION: `Support creators by helping them reach a larger market
+  Gain access to all comics for free
+  Work with creators in a legal setting
+  Searchable dialogue/manuscript
+  Built in glossary for changing teams`,
+  LABEL_FEATURES: 'Features',
+  LABEL_FEATURES_TITLE_1: 'CrowdSourced Comic Translations',
+  LABEL_FEATURES_TITLE_1_SUB_1: 'Access to comic translators worldwide',
+  LABEL_FEATURES_TITLE_1_SUB_1_DESCRIPTION: 'Access to thousands of passionate multilingual comics translators worldwide',
+  LABEL_FEATURES_TITLE_1_SUB_2: 'Effortless collaboration in a single workspace',
   LABEL_FEATURES_TITLE_1_SUB_2_DESCRIPTION: `Use only one tool to another for different tasks. Translators, typesetters and proofreaders can easily participate in reviewing each other's work, picking up the work where they need to in the same workspace.`,
   LABEL_FEATURES_TITLE_2: 'Specialized Tools For Managing Translations',
   LABEL_FEATURES_TITLE_2_SUB_1: ' Powerful tools that run on any browser',
@@ -43,20 +39,17 @@ const landingPage = {
   LABEL_FEATURES_TITLE_2_SUB_2_DESCRIPTION: `Annotated commenting let translators, proofreaders and
   typesetters communicate with each other`,
   LABEL_FEATURES_TITLE_2_SUB_3: ' Centralized comics glossaries',
-  LABEL_FEATURES_TITLE_2_SUB_3_DESCRIPTION: `Glosari komik terus diperbarui untuk memastikan
-    konsistensi dan kualitas terjemahan yang mencakup
-    bab yang berbeda. Glosarium membantu staf baru untuk melompat ke kanan
-    di.`,
-  LABEL_FEATURES_TITLE_2_SUB_4: 'Kontrol atas apa yang dipublikasikan',
-  LABEL_FEATURES_TITLE_2_SUB_4_DESCRIPTION:
-    ' Creators will always have the final cut for publishing',
+  LABEL_FEATURES_TITLE_2_SUB_3_DESCRIPTION: `Comic glossaries are constantly updated to ensure the
+    consistency and quality of the translation spanning the
+    different chapters. Glossaries helps new staff to jump right
+    in.`,
+  LABEL_FEATURES_TITLE_2_SUB_4: 'Control over what is published',
+  LABEL_FEATURES_TITLE_2_SUB_4_DESCRIPTION: ' Creators will always have the final cut for publishing',
   LABEL_FEATURES_TITLE_3: 'Support Creators',
   LABEL_FEATURES_TITLE_3_SUB_1: 'Support by Subscribing',
-  LABEL_FEATURES_TITLE_3_SUB_1_DESCRIPTION:
-    'Easy monetization - Control which pages are free and which you want to charge for. We offer the industry’s best revenue share rates with transparent and immediate payouts. Monetize from day one Instant Payout - no minumums',
+  LABEL_FEATURES_TITLE_3_SUB_1_DESCRIPTION: 'Easy monetization - Control which pages are free and which you want to charge for. We offer the industry’s best revenue share rates with transparent and immediate payouts. Monetize from day one Instant Payout - no minumums',
   LABEL_FEATURES_TITLE_3_SUB_2: 'Support by Translating',
-  LABEL_FEATURES_TITLE_3_SUB_2_DESCRIPTION:
-    'more translations = more readers more readers = more income to creators more income = more comics!',
+  LABEL_FEATURES_TITLE_3_SUB_2_DESCRIPTION: 'more translations = more readers more readers = more income to creators more income = more comics!',
   LABEL_FEATURES_TITLE_3_SUB_3: 'Support by Reading and Sharing',
   LABEL_FEATURES_TITLE_3_SUB_3_DESCRIPTION: ` Creators earn through Ad revenue sharing For every page read,
   a portion of the Ad revenues will go to the creators`,
@@ -65,7 +58,6 @@ const landingPage = {
   sharing`,
   LABEL_CREATOR: 'Creator',
   LABEL_EPICO: 'epico',
-  LABEL_FEATURED_SERIES: 'Featured Series',
+  LABEL_FEATURED_SERIES: 'Featured Series'
 };
-
 export default landingPage;
