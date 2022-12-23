92ba1184c0314c6c197b8a715940434585cb9928
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:21:20 +0800

    New translations search.ts (Filipino)

--- crowdin-translate/fil/search.ts
+++ crowdin-translate/fil/search.ts
@@ -2,15 +2,12 @@ import topNav from './commons/topNav';
 import common from './commons/common';
 import authModal from './commons/authModal';
 import genres from './commons/genres';
-
-const search = {
-  ...topNav,
+const search = { ...topNav,
   ...common,
   ...authModal,
   ...genres,
-  LABEL_SEARCH_PLACEHOLDER: 'Maghanap ng mga serye at tagalikha',
-  LABEL_MORE_FETCHING_CONTENTS: 'Kinukuha ang higit pang nilalaman...',
-  LABEL_MORE_SEARCHING_CONTENTS: 'Naghahanap ng mga nilalaman...',
+  LABEL_SEARCH_PLACEHOLDER: 'Search for series and creators',
+  LABEL_MORE_FETCHING_CONTENTS: 'Fetching more contents...',
+  LABEL_MORE_SEARCHING_CONTENTS: 'Searching for contents...'
 };
-
 export default search;
