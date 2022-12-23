3e6c93f28c4896c10a1b29270bf30e2385ad762f
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:19:27 +0800

    New translations seriesReader.ts (Filipino)

--- crowdin-translate/fil/commons/seriesReader.ts
+++ crowdin-translate/fil/commons/seriesReader.ts
@@ -1,7 +1,6 @@
 const seriesReader = {
-  LABEL_LOADING_CHAPTER: 'Nilo-load ang kabanata...',
-  LABEL_NO_CHAPTER_SELECTED: 'Walang napiling kabanata',
-  LABEL_NO_PERMISSION_TO_VIEW: 'Walang pahintulot na tingnan',
+  LABEL_LOADING_CHAPTER: 'Loading chapter...',
+  LABEL_NO_CHAPTER_SELECTED: 'No chapter selected',
+  LABEL_NO_PERMISSION_TO_VIEW: 'No permission to view'
 };
-
 export default seriesReader;
