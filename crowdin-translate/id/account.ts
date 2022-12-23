084a2fccb4272d800dd45aecaf6b70cb99d82f05
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:18:19 +0800

    New translations account.ts (Indonesian)

--- crowdin-translate/id/account.ts
+++ crowdin-translate/id/account.ts
@@ -1,14 +1,11 @@
 import topNav from './commons/topNav';
 import common from './commons/common';
 import authModal from './commons/authModal';
-
-const account = {
-  ...topNav,
+const account = { ...topNav,
   ...common,
   ...authModal,
-  LABEL_SIGN_IN: 'Masuk',
-  LABEL_SIGN_OUT: 'Keluar',
-  LABEL_SETTINGS: 'Pengaturan',
+  LABEL_SIGN_IN: 'Sign in',
+  LABEL_SIGN_OUT: 'Sign out',
+  LABEL_SETTINGS: 'Settings'
 };
-
 export default account;
