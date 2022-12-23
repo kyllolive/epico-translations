22c411263f55609fe84c7027abf5523ea88682cf
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:20:14 +0800

    New translations consoleEditChapter.ts (Marathi)

--- crowdin-translate/mr/consoleEditChapter.ts
+++ crowdin-translate/mr/consoleEditChapter.ts
@@ -1,48 +1,37 @@
 import topNav from './commons/topNav';
 import common from './commons/common';
 import imageUploader from './commons/imageUploader';
-
-const consoleEditChapter = {
-  ...common,
+const consoleEditChapter = { ...common,
   ...topNav,
   ...imageUploader,
-  LABEL_SERIES: 'मालिका',
-  LABEL_CHAPTER: 'धडा',
-  LABEL_EDIT: 'सुधारणे',
-  LABEL_EDIT_CHAPTER: 'प्रकरण संपादित करा',
-  LABEL_SUBMIT: 'प्रस्तुत करणे',
-  LABEL_UPDATE: 'अपडेट करा',
+  LABEL_SERIES: 'series',
+  LABEL_CHAPTER: 'chapter',
+  LABEL_EDIT: 'Edit',
+  LABEL_EDIT_CHAPTER: 'Edit Chapter',
+  LABEL_SUBMIT: 'Submit',
+  LABEL_UPDATE: 'Update',
   // FORM VALIDATION
-  FORM_VALIDATION_COVER_REQUIRED: 'कव्हर इमेज आवश्यक आहे!',
-  FORM_VALIDATION_CHAPTER_DESCRIPTION_MAX:
-    'प्रकरणाचे वर्णन जास्तीत जास्त २५०० वर्णांचे असणे आवश्यक आहे',
-  FORM_VALIDATION_CHAPTER_DESCRIPTION_REQUIRED: 'प्रकरणाचे वर्णन आवश्यक आहे',
-  FORM_VALIDATION_CHAPTER_NOTE_MAX:
-    'चॅप्टर नोट कमाल २५०० वर्णांची असणे आवश्यक आहे',
-  FORM_VALIDATION_CHAPTER_PUBLISHED_REQUIRED: 'प्रकाशित करणे आवश्यक आहे',
-  FORM_VALIDATION_CHAPTER_TITLE_MAX:
-    'अध्याय शीर्षक जास्तीत जास्त 100 वर्णांचे असणे आवश्यक आहे',
-  FORM_VALIDATION_CHAPTER_TITLE_MATCHES:
-    'क्षमस्व, फक्त अक्षरे (a-z), संख्या (0-9), विशेष वर्ण (\':?"!-_&()<>.,+=@#$%|) अनुमत आहेत.',
-  FORM_VALIDATION_CHAPTER_TITLE_REQUIRED: 'अध्याय शीर्षक आवश्यक आहे',
-  FORM_VALIDATION_CHAPTER_PAGES_MIN: 'किमान 1 प्रतिमा असणे आवश्यक आहे',
+  FORM_VALIDATION_COVER_REQUIRED: 'Cover image is required!',
+  FORM_VALIDATION_CHAPTER_DESCRIPTION_MAX: 'Chapter description must be at most 2500 characters',
+  FORM_VALIDATION_CHAPTER_DESCRIPTION_REQUIRED: 'Chapter description is required',
+  FORM_VALIDATION_CHAPTER_NOTE_MAX: 'Chapter note must be at most 2500 characters',
+  FORM_VALIDATION_CHAPTER_PUBLISHED_REQUIRED: 'Published is required',
+  FORM_VALIDATION_CHAPTER_TITLE_MAX: 'Chapter title must be at most 100 characters',
+  FORM_VALIDATION_CHAPTER_TITLE_MATCHES: 'Sorry, only letters (a-z), numbers (0-9), special characters (\':?"!-_&()<>.,+=@#$%|) are allowed.',
+  FORM_VALIDATION_CHAPTER_TITLE_REQUIRED: 'Chapter title is required',
+  FORM_VALIDATION_CHAPTER_PAGES_MIN: 'Must contain atleast 1 image',
   // END FORM VALIDATION
-  LABEL_SINGLE_COVER_DROPBOX:
-    'तुमचा कव्हर फोटो येथे टाका किंवा ब्राउझ करण्यासाठी क्लिक करा',
-  LABEL_SINGLE_CHAPTER_COVER_SUB:
-    'सर्व उपकरणांवरील सर्वोत्तम परिणामांसाठी, किमान 360 x 360 पिक्सेल असलेली प्रतिमा वापरा',
-  LABEL_PUBLISH_CHAPTER: 'अध्याय प्रकाशित करा',
-  LABEL_CHAPTER_TITLE: 'अध्याय शीर्षक',
-  LABEL_CHAPTER_DESCRIPTION: 'अध्याय वर्णन',
-  LABEL_CHAPTER_NOTE_OPTIONAL: 'धडा टीप (पर्यायी)',
-  LABEL_NOTE_TEXTAREA_MSG_BOTTOM: 'ही नोंद अध्यायाच्या शेवटी दर्शविली जाईल',
-  LABEL_UPLOAD_PAGES: 'पृष्ठे अपलोड करा',
-  LABEL_NOT_FOUND_HEADER:
-    'अरेरे, आम्ही संपादनासाठी हा अध्याय शोधण्यात अक्षम होतो',
-  LABEL_NOT_FOUND_MSG_1:
-    'जर तुम्हाला वाटत असेल की हा एपिको चॅप्टर अस्तित्वात आहे, तर कृपया दिलेल्या सीरिज आयडीसह आमच्या समर्थनाशी संपर्क साधा',
-  LABEL_NOT_FOUND_MSG_2: 'च्या भाषा आयडीसह',
-  LABEL_NOT_FOUND_MSG_3: 'आणि चा अध्याय आयडी',
+  LABEL_SINGLE_COVER_DROPBOX: 'Drop your cover photo here, or click to browse',
+  LABEL_SINGLE_CHAPTER_COVER_SUB: 'For the best results on all devices, use an image that’s at least 360 x 360 pixels',
+  LABEL_PUBLISH_CHAPTER: 'Publish chapter',
+  LABEL_CHAPTER_TITLE: 'Chapter title',
+  LABEL_CHAPTER_DESCRIPTION: 'Chapter description',
+  LABEL_CHAPTER_NOTE_OPTIONAL: 'Chapter note (optional)',
+  LABEL_NOTE_TEXTAREA_MSG_BOTTOM: 'This note will be shown at the end of the chapter',
+  LABEL_UPLOAD_PAGES: 'Upload pages',
+  LABEL_NOT_FOUND_HEADER: 'Uh oh, we were unable to find this chapter for edit',
+  LABEL_NOT_FOUND_MSG_1: 'If you think this epico chapter exists kindly contact our support with the given series ID',
+  LABEL_NOT_FOUND_MSG_2: 'with language id of',
+  LABEL_NOT_FOUND_MSG_3: 'and chapter ID of'
 };
-
 export default consoleEditChapter;
