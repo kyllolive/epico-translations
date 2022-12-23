ca9db4fbc081387b0ab39925d800ef5acd561420
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:19:59 +0800

    New translations consoleCreateChapter.ts (Cebuano)

--- crowdin-translate/ceb/consoleCreateChapter.ts
+++ crowdin-translate/ceb/consoleCreateChapter.ts
@@ -1,50 +1,36 @@
 import topNav from './commons/topNav';
 import common from './commons/common';
 import imageUploader from './commons/imageUploader';
-
-const consoleCreateChapter = {
-  ...common,
+const consoleCreateChapter = { ...common,
   ...topNav,
   ...imageUploader,
-  LABEL_SERIES: 'sunod-sunod nga',
-  LABEL_CHAPTER: 'kapitulo',
-  LABEL_SOMETHING_WENT_WRONG:
-    'Oops naay nahitabo, palihog sulayi pag-usab unya!',
-  LABEL_GO_BACK: 'Balik',
-  LABEL_ADD_NEW_SERIES: 'Idugang ang Bag-ong Serye',
-  LABEL_NEXT_CHAPTER: 'sunod: kapitulo',
-  LABEL_SUBMIT: 'Isumite',
+  LABEL_SERIES: 'series',
+  LABEL_CHAPTER: 'chapter',
+  LABEL_SOMETHING_WENT_WRONG: 'Oops something went wrong, please try again later!',
+  LABEL_GO_BACK: 'Go Back',
+  LABEL_ADD_NEW_SERIES: 'Add New Series',
+  LABEL_NEXT_CHAPTER: 'next: chapter',
+  LABEL_SUBMIT: 'Submit',
   // FORM VALIDATION
-  FORM_VALIDATION_COVER_REQUIRED: 'Kinahanglan ang cover image!',
-  FORM_VALIDATION_CHAPTER_DESCRIPTION_MAX:
-    'Ang paghulagway sa kapitulo kinahanglan nga labing daghan sa 2500 ka mga karakter',
-  FORM_VALIDATION_CHAPTER_DESCRIPTION_REQUIRED:
-    'Ang paghulagway sa kapitulo gikinahanglan',
-  FORM_VALIDATION_CHAPTER_NOTE_MAX:
-    'Ang nota sa kapitulo kinahanglan nga labing daghan sa 2500 ka mga karakter',
-  FORM_VALIDATION_CHAPTER_PUBLISHED_REQUIRED: 'Gikinahanglan ang pagpatik',
-  FORM_VALIDATION_CHAPTER_TITLE_MAX:
-    'Ang ulohan sa kapitulo kinahanglang labing daghan sa 100 ka karakter',
-  FORM_VALIDATION_CHAPTER_TITLE_MATCHES:
-    'Pasensya, mga letra (a-z), numero (0-9), espesyal nga karakter (\':?"!-_&()<>.,+=@#$%|) ang gitugotan.',
-  FORM_VALIDATION_CHAPTER_TITLE_REQUIRED:
-    'Ang ulohan sa kapitulo gikinahanglan',
-  FORM_VALIDATION_CHAPTER_PAGES_MIN:
-    'Kinahanglan adunay labing menos 1 nga imahe',
+  FORM_VALIDATION_COVER_REQUIRED: 'Cover image is required!',
+  FORM_VALIDATION_CHAPTER_DESCRIPTION_MAX: 'Chapter description must be at most 2500 characters',
+  FORM_VALIDATION_CHAPTER_DESCRIPTION_REQUIRED: 'Chapter description is required',
+  FORM_VALIDATION_CHAPTER_NOTE_MAX: 'Chapter note must be at most 2500 characters',
+  FORM_VALIDATION_CHAPTER_PUBLISHED_REQUIRED: 'Published is required',
+  FORM_VALIDATION_CHAPTER_TITLE_MAX: 'Chapter title must be at most 100 characters',
+  FORM_VALIDATION_CHAPTER_TITLE_MATCHES: 'Sorry, only letters (a-z), numbers (0-9), special characters (\':?"!-_&()<>.,+=@#$%|) are allowed.',
+  FORM_VALIDATION_CHAPTER_TITLE_REQUIRED: 'Chapter title is required',
+  FORM_VALIDATION_CHAPTER_PAGES_MIN: 'Must contain atleast 1 image',
   // END FORM VALIDATION
-  LABEL_CREATE_CHAPTER: 'Paghimo og Kapitulo',
-  LABEL_SINGLE_COVER_DROPBOX:
-    'Ihulog ang imong litrato sa hapin dinhi, o i-klik aron mag-browse',
-  LABEL_SINGLE_CHAPTER_COVER_SUB:
-    'Para sa pinakanindot nga resulta sa tanang device, gamita ang hulagway nga labing menos 360 x 360 pixels',
-  LABEL_SERIES_DESCRIPTION: 'Deskripsyon sa serye',
-  LABEL_PUBLISH_CHAPTER: 'Imantala ang kapitulo',
-  LABEL_CHAPTER_TITLE: 'Ulohan sa kapitulo',
-  LABEL_CHAPTER_DESCRIPTION: 'Deskripsyon sa kapitulo',
-  LABEL_CHAPTER_NOTE_OPTIONAL: 'nota sa kapitulo (opsyonal)',
-  LABEL_NOTE_TEXTAREA_MSG_BOTTOM:
-    'Kini nga mubo nga sulat ipakita sa katapusan sa kapitulo',
-  LABEL_UPLOAD_PAGES: 'Pag-upload og mga panid',
+  LABEL_CREATE_CHAPTER: 'Create Chapter',
+  LABEL_SINGLE_COVER_DROPBOX: 'Drop your cover photo here, or click to browse',
+  LABEL_SINGLE_CHAPTER_COVER_SUB: 'For the best results on all devices, use an image that’s at least 360 x 360 pixels',
+  LABEL_SERIES_DESCRIPTION: 'Series description',
+  LABEL_PUBLISH_CHAPTER: 'Publish chapter',
+  LABEL_CHAPTER_TITLE: 'Chapter title',
+  LABEL_CHAPTER_DESCRIPTION: 'Chapter description',
+  LABEL_CHAPTER_NOTE_OPTIONAL: 'Chapter note (optional)',
+  LABEL_NOTE_TEXTAREA_MSG_BOTTOM: 'This note will be shown at the end of the chapter',
+  LABEL_UPLOAD_PAGES: 'Upload pages'
 };
-
 export default consoleCreateChapter;
