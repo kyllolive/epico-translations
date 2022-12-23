a5aeeb23e710a9217a91a46ee7fafafa7bed9c58
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:18:13 +0800

    New translations about.ts (Filipino)

--- crowdin-translate/fil/about.ts
+++ crowdin-translate/fil/about.ts
@@ -1,24 +1,17 @@
 import topNav from './commons/topNav';
 import common from './commons/common';
 import authModal from './commons/authModal';
-
-const home = {
-  ...topNav,
+const home = { ...topNav,
   ...common,
   ...authModal,
-  LABEL_ABOUT_US_HEADER_5: 'TUNGKOL SA ATIN',
-  LABEL_ABOUT_US_BODY_5_1:
-    'Sa pamamagitan man ng teksto o mga ilustrasyon, naniniwala kaming lahat ay may kwentong ibabahagi.',
-  LABEL_ABOUT_US_BODY_5_2:
-    'Naniniwala din kami na ang mga mahuhusay na komiks ay maaaring magmula sa kahit saan.',
-  LABEL_ABOUT_US_BODY_5_3:
-    'Kami ay isang pangkat ng mga tagahanga ng komiks na determinadong bumuo ng mga tool',
-  LABEL_ABOUT_US_BODY_5_4:
-    "upang matulungan ang mga tagalikha ng komiks na ikonekta ang kanilang mga kuwento sa pinakamaraming tao hangga't maaari.",
-  LABEL_ABOUT_US_HEADER_6: 'ANO ANG KWENTO MO?',
-  LABEL_SIGN_UP: 'Mag-sign up',
-  LABEL_CREATE_SERIES: 'Gumawa ng serye',
-  LABEL_MY_PROFILE: 'Aking profile',
+  LABEL_ABOUT_US_HEADER_5: 'ABOUT US',
+  LABEL_ABOUT_US_BODY_5_1: 'Be it through text or illustrations, we believe everyone has a story to share.',
+  LABEL_ABOUT_US_BODY_5_2: 'We also believe great comics can come from anywhere.',
+  LABEL_ABOUT_US_BODY_5_3: 'We are a group of comic fans determined to build tools',
+  LABEL_ABOUT_US_BODY_5_4: 'to help comic creators connect their stories to as many people as possible.',
+  LABEL_ABOUT_US_HEADER_6: "WHAT'S YOUR STORY?",
+  LABEL_SIGN_UP: 'Sign up',
+  LABEL_CREATE_SERIES: 'Create series',
+  LABEL_MY_PROFILE: 'My profile'
 };
-
 export default home;
