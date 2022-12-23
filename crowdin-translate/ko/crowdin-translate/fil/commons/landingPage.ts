b9383e887d6f3b17da9a428a13e53e6dfa27d038
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:54:53 +0800

    New translations landingPage.ts (Korean)

---
+++ crowdin-translate/ko/crowdin-translate/fil/commons/landingPage.ts
@@ -0,0 +1,63 @@
+const landingPage = {
+  LABEL_MAIN_HEADER: 'Ang Iyong Komiks ay Nararapat Mabasa Ng Mundo',
+  LABEL_MAIN_SUB_HEADER: 'Crowdsourced na serbisyo sa pagsasalin para sa komiks',
+  LABEL_PUBLISH: 'I-publish',
+  LABEL_WORKS: 'Paano ito gumagana?',
+  LABEL_WORKS_1: 'Nag-upload ang Creator ng orihinal na serye ng komiks',
+  LABEL_WORKS_2: 'Nakatuklas ng komiks series ang Translator/Cleaner',
+  LABEL_WORKS_3: 'Tinatanggal ng Cleaner ang teksto mula sa likhang sining',
+  LABEL_WORKS_4: 'Ginagawa ng Translator ang mga pagsasalin gamit ang isang tool sa pag-type ng inbrowser',
+  LABEL_WORKS_5: 'Inaprubahan ng Creator ang mga pag-edit at may panghuling mga karapatan sa pag-publish',
+  LABEL_WORKS_6: 'Natutuklasan at natutuwa ng mambabasa ang isinalin na bersyon',
+  LABEL_BENEFITS: 'Sino ang nakikinabang nito?',
+  LABEL_BENEFITOR_1: 'Mga tagalikha',
+  LABEL_BENEFITOR_1_DESCRIPTION: `Dagdagan ang abot/audience/fans
+  Pinakamadaling paraan upang maisalin ang iyong mga komiks
+  Tumanggap ng tulong nang walang kalakip na mga string
+  Kumita sa tuwing binabasa ang iyong mga komiks
+  Makatipid ng oras at pera sa mga pagsasalin.`,
+  LABEL_BENEFITOR_2: 'Mga mambabasa',
+  LABEL_BENEFITOR_2_DESCRIPTION: `Walang limitasyong access sa lahat ng komiks, graphic novel, manhwa at manga mula sa buong mundo
+  Suportahan ang mga tagalikha sa pamamagitan ng buwanang mga donasyon. Hinahati namin ang lahat ng donasyon sa mga pamagat na nabasa mo.`,
+  LABEL_BENEFITOR_3: 'Mga Scanlator',
+  LABEL_BENEFITOR_3_DESCRIPTION: `Suportahan ang mga creator sa pamamagitan ng pagtulong sa kanila na maabot ang mas malaking market
+  Makakuha ng access sa lahat ng komiks nang libre
+  Makipagtulungan sa mga creator sa isang legal na setting
+  Mahahanap na dialogue/manuscript
+  Built in glossary para sa pagpapalit ng mga team`,
+  LABEL_FEATURES: 'Mga tampok',
+  LABEL_FEATURES_TITLE_1: 'Mga Pagsasalin ng CrowdSourced Comic',
+  LABEL_FEATURES_TITLE_1_SUB_1: 'Access sa mga tagasalin ng komiks sa buong mundo',
+  LABEL_FEATURES_TITLE_1_SUB_1_DESCRIPTION: 'Access sa libu-libong masigasig na mga tagasalin ng komiks sa buong mundo',
+  LABEL_FEATURES_TITLE_1_SUB_2: 'Walang hirap na pakikipagtulungan sa iisang workspace',
+  LABEL_FEATURES_TITLE_1_SUB_2_DESCRIPTION: `Gumamit lamang ng isang tool sa isa pa para sa iba\'t ibang gawain. Ang mga tagapagsalin, mga typesetter at proofreader ay madaling makilahok sa pagrepaso sa gawa ng isa\'t isa, pagkuha ng trabaho kung saan kailangan nila sa parehong workspace.`,
+  LABEL_FEATURES_TITLE_2: 'Mga Espesyal na Tool Para sa Pamamahala ng Mga Pagsasalin',
+  LABEL_FEATURES_TITLE_2_SUB_1: ' Powerful tools that run on any browser',
+  LABEL_FEATURES_TITLE_2_SUB_1_DESCRIPTION: `Hindi na kailangang magbayad para sa mga tool ng third-party para sa paglilinis, pag-typeset
+    o pagbabahagi ng file.`,
+  LABEL_FEATURES_TITLE_2_SUB_2: 'Patuloy na feedback loop',
+  LABEL_FEATURES_TITLE_2_SUB_2_DESCRIPTION: `Ang annotated na pagkomento ay nagpapahintulot sa mga tagapagsalin, proofreader at
+  ang mga typesetters ay nakikipag-usap sa isa\'t isa`,
+  LABEL_FEATURES_TITLE_2_SUB_3: ' Centralized comics glossaries',
+  LABEL_FEATURES_TITLE_2_SUB_3_DESCRIPTION: `Ang mga comic glossary ay patuloy na ina-update upang matiyak ang
+    pagkakapare-pareho at kalidad ng pagsasalin na sumasaklaw sa
+    iba\'t ibang kabanata. Tinutulungan ng mga glossary ang mga bagong kawani na tumalon sa kanan
+    sa.`,
+  LABEL_FEATURES_TITLE_2_SUB_4: 'Kontrol sa kung ano ang nai-publish',
+  LABEL_FEATURES_TITLE_2_SUB_4_DESCRIPTION: ' Creators will always have the final cut for publishing',
+  LABEL_FEATURES_TITLE_3: 'Suportahan ang Mga Tagalikha',
+  LABEL_FEATURES_TITLE_3_SUB_1: 'Suporta sa pamamagitan ng Pag-subscribe',
+  LABEL_FEATURES_TITLE_3_SUB_1_DESCRIPTION: 'Easy monetization - Kontrolin kung aling mga page ang libre at kung saan mo gustong singilin. Nag-aalok kami ng pinakamahusay na mga rate ng pagbabahagi ng kita sa industriya na may malinaw at agarang mga payout. Mag-monetize mula sa unang araw na Instant na Payout - walang mga minimum',
+  LABEL_FEATURES_TITLE_3_SUB_2: 'Suporta sa pamamagitan ng Pagsasalin',
+  LABEL_FEATURES_TITLE_3_SUB_2_DESCRIPTION: 'more translations = more readers more readers = more income to creators more income = more comics!',
+  LABEL_FEATURES_TITLE_3_SUB_3: 'Suporta sa pamamagitan ng Pagbasa at Pagbabahagi',
+  LABEL_FEATURES_TITLE_3_SUB_3_DESCRIPTION: ` Creators earn through Ad revenue sharing For every page read,
+  a portion of the Ad revenues will go to the creators`,
+  LABEL_PIE_ROYALTY_RATE: `Pinakamataas na royalty rate ng industriya na 95%`,
+  LABEL_PIE_TEXT_CONTENT: `I-monetize ang iyong mga view mula sa buwanang subscription at mga kita sa ad
+  pagbabahagi`,
+  LABEL_CREATOR: 'Tagapaglikha',
+  LABEL_EPICO: 'epico',
+  LABEL_FEATURED_SERIES: 'Tampok na Serye'
+};
+export default landingPage;
