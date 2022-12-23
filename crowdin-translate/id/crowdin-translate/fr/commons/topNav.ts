f4eeb899ee8a4ee4c1a51104286cf30b5aef2803
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:23:45 +0800

    New translations topNav.ts (Indonesian)

---
+++ crowdin-translate/id/crowdin-translate/fr/commons/topNav.ts
@@ -0,0 +1,20 @@
+const topNav = {
+  LABEL_HOME: 'Maison',
+  LABEL_GENRE: 'Genre',
+  LABEL_TRANSLATE: 'Scanlate',
+  LABEL_NOTIFICATION: 'Avis',
+  LABEL_ACCOUNT: 'Mon compte',
+  LABEL_ABOUT_US: 'À propos de nous',
+  LABEL_SIGN_IN: "S'identifier",
+  LABEL_SIGN_OUT: 'Déconnexion',
+  LABEL_SETTINGS: 'Réglages',
+  LABEL_SEARCH: 'Chercher',
+  LABEL_PUBLISH: 'Publier',
+  TOOLTIP_CREATE_SERIES: 'Créer une série',
+  TOOLTIP_SERIES_LIST: 'Liste des séries',
+  TOOLTIP_MENU: 'Menu',
+  LABEL_SIGNED_IN_AS: 'Signed in as',
+  LABEL_MY_PROFILE: 'My Profile',
+  LABEL_MY_ACCOUNT_SETTINGS: 'My Account Setting'
+};
+export default topNav;
