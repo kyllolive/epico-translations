1ed0364f02f2a4becfab0dc6da98f939b416424b
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:19:30 +0800

    New translations tagForm.ts (Japanese)

--- crowdin-translate/ja/commons/tagForm.ts
+++ crowdin-translate/ja/commons/tagForm.ts
@@ -1,6 +1,5 @@
 const tagForm = {
-  LABEL_TAG_TEXTFIELD: 'タグ (オプション - 最大 5)',
-  LABEL_TAG_PLACEHOLDER: 'タグ',
+  LABEL_TAG_TEXTFIELD: 'Tags(optional - max 5)',
+  LABEL_TAG_PLACEHOLDER: 'Tags'
 };
-
 export default tagForm;
