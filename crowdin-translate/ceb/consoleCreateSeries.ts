cd2a3205679be4a69f449f40f0884d5b21ca686f
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:20:09 +0800

    New translations consoleCreateSeries.ts (Cebuano)

--- crowdin-translate/ceb/consoleCreateSeries.ts
+++ crowdin-translate/ceb/consoleCreateSeries.ts
@@ -3,85 +3,61 @@ import common from './commons/common';
 import imageUploader from './commons/imageUploader';
 import genres from './commons/genres';
 import tagForm from './commons/tagForm';
-
-const consoleCreateSeries = {
-  ...genres,
+const consoleCreateSeries = { ...genres,
   ...common,
   ...topNav,
   ...imageUploader,
   ...tagForm,
-  LABEL_SERIES: 'sunod-sunod nga',
-  LABEL_CHAPTER: 'kapitulo',
-  LABEL_SOMETHING_WENT_WRONG:
-    'Oops naay nahitabo, palihog sulayi pag-usab unya!',
-  LABEL_GO_BACK: 'Balik',
-  LABEL_ADD_NEW_SERIES: 'Idugang ang Bag-ong Serye',
-  LABEL_NEXT_CHAPTER: 'sunod: kapitulo',
-  LABEL_SUBMIT: 'Isumite',
+  LABEL_SERIES: 'series',
+  LABEL_CHAPTER: 'chapter',
+  LABEL_SOMETHING_WENT_WRONG: 'Oops something went wrong, please try again later!',
+  LABEL_GO_BACK: 'Go Back',
+  LABEL_ADD_NEW_SERIES: 'Add New Series',
+  LABEL_NEXT_CHAPTER: 'next: chapter',
+  LABEL_SUBMIT: 'Submit',
   // FORM VALIDATION
-  FORM_VALIDATION_DESCRIPTION_MAX:
-    'Ang titulo sa deskripsyon sa serye kinahanglang labing daghan ug 2500 ka karakter',
-  FORM_VALIDATION_DESCRIPTION_REQUIRED:
-    'Ang deskripsyon sa serye gikinahanglan',
-  FORM_VALIDATION_GENRE_REQUIRED: 'Gikinahanglan ang genre',
-  FORM_VALIDATION_GENRE_MAX:
-    'Ang natad sa genre kinahanglang adunay ubos o katumbas sa 3 ka mga butang',
-  FORM_VALIDATION_LANGUAGE_ID_REQUIRED: 'Ang pinulongan gikinahanglan',
-  FORM_VALIDATION_TITLE_MAX:
-    'Ang titulo sa serye kinahanglan labing taas sa 100 ka karakter',
-  FORM_VALIDATION_TITLE_MATCHES:
-    'Pasensya, mga letra (a-z), numero (0-9), espesyal nga karakter (\':?"!-_&()<>.,+=@#$%|) ang gitugotan.',
-  FORM_VALIDATION_TITLE_REQUIRED: 'Ang titulo sa serye gikinahanglan',
-  FORM_VALIDATION_COVER_REQUIRED: 'Kinahanglan ang cover image!',
-  FORM_VALIDATION_BANNER_REQUIRED: 'Ang hulagway sa banner gikinahanglan!',
-  FORM_VALIDATION_CHAPTER_DESCRIPTION_MAX:
-    'Ang paghulagway sa kapitulo kinahanglan nga labing daghan sa 2500 ka mga karakter',
-  FORM_VALIDATION_CHAPTER_DESCRIPTION_REQUIRED:
-    'Ang paghulagway sa kapitulo gikinahanglan',
-  FORM_VALIDATION_CHAPTER_NOTE_MAX:
-    'Ang nota sa kapitulo kinahanglan nga labing daghan sa 2500 ka mga karakter',
-  FORM_VALIDATION_CHAPTER_PUBLISHED_REQUIRED: 'Gikinahanglan ang pagpatik',
-  FORM_VALIDATION_CHAPTER_TITLE_MAX:
-    'Ang ulohan sa kapitulo kinahanglang labing daghan sa 100 ka karakter',
-  FORM_VALIDATION_CHAPTER_TITLE_MATCHES:
-    'Pasensya, mga letra (a-z), numero (0-9), espesyal nga karakter (\':?"!-_&()<>.,+=@#$%|) ang gitugotan.',
-  FORM_VALIDATION_CHAPTER_TITLE_REQUIRED:
-    'Ang ulohan sa kapitulo gikinahanglan',
-  FORM_VALIDATION_CHAPTER_PAGES_MIN:
-    'Kinahanglan adunay labing menos 1 nga imahe',
+  FORM_VALIDATION_DESCRIPTION_MAX: 'Series description title must be at most 2500 characters',
+  FORM_VALIDATION_DESCRIPTION_REQUIRED: 'Series description is required',
+  FORM_VALIDATION_GENRE_REQUIRED: 'Genre is required',
+  FORM_VALIDATION_GENRE_MAX: 'Genre field must have less than or equal to 3 items',
+  FORM_VALIDATION_LANGUAGE_ID_REQUIRED: 'Language is required',
+  FORM_VALIDATION_TITLE_MAX: 'Series title must be at most 100 characters',
+  FORM_VALIDATION_TITLE_MATCHES: 'Sorry, only letters (a-z), numbers (0-9), special characters (\':?"!-_&()<>.,+=@#$%|) are allowed.',
+  FORM_VALIDATION_TITLE_REQUIRED: 'Series title is required',
+  FORM_VALIDATION_COVER_REQUIRED: 'Cover image is required!',
+  FORM_VALIDATION_BANNER_REQUIRED: 'Banner image is required!',
+  FORM_VALIDATION_CHAPTER_DESCRIPTION_MAX: 'Chapter description must be at most 2500 characters',
+  FORM_VALIDATION_CHAPTER_DESCRIPTION_REQUIRED: 'Chapter description is required',
+  FORM_VALIDATION_CHAPTER_NOTE_MAX: 'Chapter note must be at most 2500 characters',
+  FORM_VALIDATION_CHAPTER_PUBLISHED_REQUIRED: 'Published is required',
+  FORM_VALIDATION_CHAPTER_TITLE_MAX: 'Chapter title must be at most 100 characters',
+  FORM_VALIDATION_CHAPTER_TITLE_MATCHES: 'Sorry, only letters (a-z), numbers (0-9), special characters (\':?"!-_&()<>.,+=@#$%|) are allowed.',
+  FORM_VALIDATION_CHAPTER_TITLE_REQUIRED: 'Chapter title is required',
+  FORM_VALIDATION_CHAPTER_PAGES_MIN: 'Must contain atleast 1 image',
   // END FORM VALIDATION
-  LABEL_SINGLE_BANNER_DROPBOX:
-    'Ihulog ang imong banner dinhi, o i-klik aron ma-browse.',
-  LABEL_SINGLE_COVER_DROPBOX:
-    'Ihulog ang imong litrato sa hapin dinhi, o i-klik aron mag-browse.',
-  LABEL_SINGLE_CHAPTER_COVER:
-    'Ihulog ang imong litrato sa hapin dinhi, o i-klik aron mag-browse.',
-  LABEL_SINGLE_BANNER_SUB:
-    'Para sa labing maayong resulta sa tanang device, gamita ang hulagway nga labing menos 1080 x 360 pixels.',
-  LABEL_SINGLE_COVER_SUB:
-    'Para sa labing maayong resulta sa tanang device, gamita ang hulagway nga labing menos 600 x 400 pixels.',
-  LABEL_SINGLE_CHAPTER_COVER_SUB:
-    'Para sa pinakanindot nga resulta sa tanang device, gamita ang hulagway nga labing menos 360 x 360 pixels.',
-  LABEL_SERIES_TITLE: 'Titulo sa serye',
-  LABEL_LANGUAGE: 'Mga pinulongan',
-  LABEL_PRIMARY_GENRE: 'Panguna nga genre',
-  LABEL_SECONDARY_GENRE: 'Ikaduha nga genre',
-  LABEL_SERIES_DESCRIPTION: 'Deskripsyon sa serye',
-  LABEL_PUBLISH_CHAPTER: 'Imantala ang kapitulo',
-  LABEL_CHAPTER_TITLE: 'Ulohan sa kapitulo',
-  LABEL_CHAPTER_DESCRIPTION: 'Deskripsyon sa kapitulo',
-  LABEL_CHAPTER_NOTE_OPTIONAL: 'nota sa kapitulo (opsyonal)',
-  LABEL_NOTE_TEXTAREA_MSG_BOTTOM:
-    'Kini nga mubo nga sulat ipakita sa katapusan sa kapitulo',
-  LABEL_UPLOAD_PAGES: 'Pag-upload og mga panid',
-  LABEL_CONGRATULATION: 'Mga pahalipay',
-  LABEL_SUCCESS_CREATING_SERIES_MSG:
-    'Malampuson nimong nahimo ang imong serye.',
-  LABEL_ADD_CHAPTER: 'dugangi ang kapitulo',
-  LABEL_CREATE_NEW_SERIES: 'paghimo og bag-ong serye',
-  LABEL_MAX_GENRES: 'Makapili ka hangtod sa 3 ka genre',
-  LABEL_OPTIONAL_TAG_ERROR_MESSAGE: 'kay reserba nga pulong, dili magamit!',
-  LABEL_ALLOW_FOR_TRANSLATION: 'Tugoti ang CrowdSourced Translation',
+  LABEL_SINGLE_BANNER_DROPBOX: 'Drop your banner here, or click to browse.',
+  LABEL_SINGLE_COVER_DROPBOX: 'Drop your cover photo here, or click to browse.',
+  LABEL_SINGLE_CHAPTER_COVER: 'Drop your cover photo here, or click to browse.',
+  LABEL_SINGLE_BANNER_SUB: 'For the best results on all devices, use an image that’s at least 1080 x 360 pixels.',
+  LABEL_SINGLE_COVER_SUB: 'For the best results on all devices, use an image that’s at least 600 x 400 pixels.',
+  LABEL_SINGLE_CHAPTER_COVER_SUB: 'For the best results on all devices, use an image that’s at least 360 x 360 pixels.',
+  LABEL_SERIES_TITLE: 'Series title',
+  LABEL_LANGUAGE: 'Languages',
+  LABEL_PRIMARY_GENRE: 'Primary genre',
+  LABEL_SECONDARY_GENRE: 'Secondary genre',
+  LABEL_SERIES_DESCRIPTION: 'Series description',
+  LABEL_PUBLISH_CHAPTER: 'Publish chapter',
+  LABEL_CHAPTER_TITLE: 'Chapter title',
+  LABEL_CHAPTER_DESCRIPTION: 'Chapter description',
+  LABEL_CHAPTER_NOTE_OPTIONAL: 'Chapter note (optional)',
+  LABEL_NOTE_TEXTAREA_MSG_BOTTOM: 'This note will be shown at the end of the chapter',
+  LABEL_UPLOAD_PAGES: 'Upload pages',
+  LABEL_CONGRATULATION: 'Congratulations',
+  LABEL_SUCCESS_CREATING_SERIES_MSG: 'You have successfully created your series.',
+  LABEL_ADD_CHAPTER: 'add chapter',
+  LABEL_CREATE_NEW_SERIES: 'create new series',
+  LABEL_MAX_GENRES: 'You can choose up to 3 genres',
+  LABEL_OPTIONAL_TAG_ERROR_MESSAGE: 'is a reserve word, cannot be used!',
+  LABEL_ALLOW_FOR_TRANSLATION: 'Allow CrowdSourced Translation'
 };
-
 export default consoleCreateSeries;
