ea4425db6fa71051fb0069c25ee1df1089909a20
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:21:11 +0800

    New translations profile.ts (Hindi)

--- crowdin-translate/hi/profile.ts
+++ crowdin-translate/hi/profile.ts
@@ -3,26 +3,23 @@ import topNav from './commons/topNav';
 import common from './commons/common';
 import authModal from './commons/authModal';
 import donationBox from './commons/donationBox';
-
-const profile = {
-  ...donationBox,
+const profile = { ...donationBox,
   ...genres,
   ...topNav,
   ...common,
   ...authModal,
-  NEW_COVER_ERROR_LABEL: 'उफ़! हम आपका नया कवर सहेजने में असमर्थ रहे',
-  LABEL_SAVE: 'बचाना',
-  LABEL_CANCEL: 'रद्द करना',
-  LABEL_SUCCESS_UPDATE_PROFILE: 'हम आपकी प्रोफ़ाइल को अपडेट करने में सफल रहे',
-  LABEL_UNABLE_SAVE_CHANGES: 'उह ओह! हम आपके परिवर्तन सहेजने में असमर्थ रहे',
-  LABEL_ADD_ANOTHER: 'एक और जोड़ें',
-  LABEL_EDIT_PROFILE: 'प्रोफ़ाइल संपादित करें',
-  LABEL_JOINED_DATE: 'शामिल होने की तारीख',
-  LABEL_UPDATE_PROFILE_INFORMATION: 'त्वरित अद्यतन प्रोफ़ाइल जानकारी',
-  LABEL_CHANGE_PROFILE_PICTURE: 'प्रोफ़ाइल चित्र बदलें',
-  LABEL_ERROR_NEW_PROFILE: 'उफ़! हम आपकी नई प्रोफ़ाइल को सहेजने में असमर्थ रहे',
-  LABEL_ERROR_NEW_COVER: 'उफ़! हम आपका नया कवर सहेजने में असमर्थ रहे',
-  LABEL_ERROR_UPLOAD_COVER: 'उफ़! हम आपका कवर अपलोड करने में असमर्थ रहे',
+  NEW_COVER_ERROR_LABEL: 'Oops! We were unable to save your new cover',
+  LABEL_SAVE: 'Save',
+  LABEL_CANCEL: 'Cancel',
+  LABEL_SUCCESS_UPDATE_PROFILE: 'We were successful updating your profile',
+  LABEL_UNABLE_SAVE_CHANGES: 'Uh oh! We were unable to save your changes',
+  LABEL_ADD_ANOTHER: 'Add Another',
+  LABEL_EDIT_PROFILE: 'Edit Profile',
+  LABEL_JOINED_DATE: 'Joined date',
+  LABEL_UPDATE_PROFILE_INFORMATION: 'Quick update profile information',
+  LABEL_CHANGE_PROFILE_PICTURE: 'Change profile picture',
+  LABEL_ERROR_NEW_PROFILE: 'Oops! We were unable to save your new profile',
+  LABEL_ERROR_NEW_COVER: 'Oops! We were unable to save your new cover',
+  LABEL_ERROR_UPLOAD_COVER: 'Oops! We were unable to upload your cover'
 };
-
 export default profile;
