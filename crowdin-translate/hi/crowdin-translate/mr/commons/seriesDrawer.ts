1611a7a9d46475800463c30f3af33cf86ccd5ca3
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:47:16 +0800

    New translations seriesDrawer.ts (Hindi)

---
+++ crowdin-translate/hi/crowdin-translate/mr/commons/seriesDrawer.ts
@@ -0,0 +1,15 @@
+const seriesDrawer = {
+  LABEL_INFORMATION_TAB_NEW_SERIES_MSG: 'नवीन अध्याय दर सोमवारी, शुक्रवारी',
+  LABEL_INFORMATION: 'माहिती',
+  LABEL_LANGUAGE: 'इंग्रजी',
+  LABEL_READ_CHAPTER_1: 'वाचा: धडा १',
+  LABEL_CREATORS: 'निर्माते',
+  LABEL_PUBLISHER: 'प्रकाशक',
+  LABEL_ARTIST: 'कलाकार',
+  LABEL_AUTHOR: 'लेखक',
+  LABEL_SEE_MORE: 'अजून पहा',
+  LABEL_PUBLISHER_OTHER_WORK: 'प्रकाशकाची इतर कामे',
+  LABEL_SERIES_YOU_MIGHT_LIKE: 'तुम्हाला आवडेल अशी मालिका',
+  LABEL_NOT_PUBLISHED_CHAPTER: 'प्रकाशित झाले नाही'
+};
+export default seriesDrawer;
