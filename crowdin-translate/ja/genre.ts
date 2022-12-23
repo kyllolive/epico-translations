79cbdfcf55a5068652b5f41e626fbfa748e08b60
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:20:42 +0800

    New translations genre.ts (Japanese)

--- crowdin-translate/ja/genre.ts
+++ crowdin-translate/ja/genre.ts
@@ -2,14 +2,11 @@ import genres from './commons/genres';
 import topNav from './commons/topNav';
 import common from './commons/common';
 import authModal from './commons/authModal';
-
-const genre = {
-  ...genres,
+const genre = { ...genres,
   ...topNav,
   ...common,
   ...authModal,
-  LABEL_LOADING_SERIES: 'シリーズを読み込んでいます...',
-  LABEL_FETCHING_MORE_SERIES: 'さらにシリーズを取得しています...',
+  LABEL_LOADING_SERIES: 'Loading series...',
+  LABEL_FETCHING_MORE_SERIES: 'Fetching more series...'
 };
-
 export default genre;
