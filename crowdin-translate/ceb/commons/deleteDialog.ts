0cd0512359fbb2ec508e8c3c9470b1f765a8a5db
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:18:06 +0800

    New translations deleteDialog.ts (Cebuano)

--- crowdin-translate/ceb/commons/deleteDialog.ts
+++ crowdin-translate/ceb/commons/deleteDialog.ts
@@ -1,14 +1,11 @@
 const deleteDialog = {
-  LABEL_WARNING_CONFIRMATION: 'Gikinahanglan ang kumpirmasyon',
-  LABEL_WARNING_REMOVE: 'Ikaw ang magtangtang',
-  LABEL_WARNING_REMOVE_MSG: 'DILI na kini mapasig-uli! SIGURADO ka ba?',
-  LABEL_DELETE_REMINDER:
-    'Kini nga aksyon mahimong mosangpot sa pagkawala sa datos. Aron malikayan ang aksidente nga mga aksyon kami naghangyo kanimo sa pagkumpirma sa imong katuyoan. Palihog type',
-  LABEL_PROCEED_CANCEL_MSG:
-    'sa pagpadayon o pagsira niini nga modala sa pagkansela.',
-  LABEL_DELETE_TITLE: 'Titulo',
-  LABEL_DELETE_CANCEL: 'Pagkanselar',
-  LABEL_DELETE_CONFIRM: 'Sa pagmatuod sa',
+  LABEL_WARNING_CONFIRMATION: 'Confirmation required',
+  LABEL_WARNING_REMOVE: 'You are going to remove',
+  LABEL_WARNING_REMOVE_MSG: 'This CANNOT be restored! Are you ABSOLUTELY sure?',
+  LABEL_DELETE_REMINDER: 'This action can lead to data loss. To prevent accidental actions we ask you to confirm your intention. Please type',
+  LABEL_PROCEED_CANCEL_MSG: 'to proceed or close this modal to cancel.',
+  LABEL_DELETE_TITLE: 'Title',
+  LABEL_DELETE_CANCEL: 'Cancel',
+  LABEL_DELETE_CONFIRM: 'Confirm'
 };
-
 export default deleteDialog;
