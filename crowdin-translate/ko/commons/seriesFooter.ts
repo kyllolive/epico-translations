175c353381ba400d9fd523d9205c0056303d4234
kyllolive <75357161+kyllolive@users.noreply.github.com>
2022-12-23 12:19:13 +0800

    New translations seriesFooter.ts (Korean)

--- crowdin-translate/ko/commons/seriesFooter.ts
+++ crowdin-translate/ko/commons/seriesFooter.ts
@@ -1,17 +1,16 @@
 const seriesFooter = {
-  LABEL_INFORMATION_TAB_NEW_SERIES_MSG: '새로운 챕터 매주 월요일, 금요일',
-  LABEL_PREVIOUS: '이전의',
-  LABEL_NEXT: '다음',
-  LABEL_COMMENTS: '코멘트',
-  LABEL_POPULAR: '인기 있는',
-  LABEL_NEWEST: '최신',
-  LABEL_REPLY: '회신하다',
-  LABEL_SHOW_MORE: '자세히보기',
-  LABEL_PUBLISHER_OTHER_WORK: '출판사의 다른 작품',
-  LABEL_SERIES_YOU_MIGHT_LIKE: '좋아할 만한 시리즈',
-  LABEL_CREATOR: '창조자',
-  LABEL_ADD_A_COMMENT: '코멘트를 추가하다',
-  LABEL_SEE_ALL: '모두 보기',
+  LABEL_INFORMATION_TAB_NEW_SERIES_MSG: 'NEW CHAPTER EVERY MONDAY, FRIDAY',
+  LABEL_PREVIOUS: 'Previous',
+  LABEL_NEXT: 'Next',
+  LABEL_COMMENTS: 'Comments',
+  LABEL_POPULAR: 'Popular',
+  LABEL_NEWEST: 'Newest',
+  LABEL_REPLY: 'Reply',
+  LABEL_SHOW_MORE: 'Show more',
+  LABEL_PUBLISHER_OTHER_WORK: "Publisher's other works",
+  LABEL_SERIES_YOU_MIGHT_LIKE: 'Series you might like',
+  LABEL_CREATOR: 'Creator',
+  LABEL_ADD_A_COMMENT: 'Add a comment',
+  LABEL_SEE_ALL: 'See all'
 };
-
 export default seriesFooter;
