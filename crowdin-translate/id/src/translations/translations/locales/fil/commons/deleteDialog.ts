9315e7529941afb9000c0f075ca35a2765307e59
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 14:04:15 +0800

    New translations deleteDialog.ts (Indonesian)

---
+++ crowdin-translate/id/src/translations/translations/locales/fil/commons/deleteDialog.ts
@@ -0,0 +1,11 @@
+const deleteDialog = {
+  LABEL_WARNING_CONFIRMATION: 'Kinakailangan ang kumpirmasyon',
+  LABEL_WARNING_REMOVE: 'Magtatanggal ka na',
+  LABEL_WARNING_REMOVE_MSG: 'HINDI na ito maibabalik! Sigurado ka ba?',
+  LABEL_DELETE_REMINDER: 'Ang pagkilos na ito ay maaaring humantong sa pagkawala ng data. Upang maiwasan ang mga hindi sinasadyang pagkilos, hinihiling namin sa iyo na kumpirmahin ang iyong intensyon. Mangyaring i-type',
+  LABEL_PROCEED_CANCEL_MSG: 'upang magpatuloy o isara ang modal na ito upang kanselahin.',
+  LABEL_DELETE_TITLE: 'Pamagat',
+  LABEL_DELETE_CANCEL: 'Kanselahin',
+  LABEL_DELETE_CONFIRM: 'Kumpirmahin'
+};
+export default deleteDialog;
