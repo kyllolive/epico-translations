c4a4d077849ad8302ad7a9fc159423087bb1a1f0
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 14:13:06 +0800

    New translations donationBox.ts (Hindi)

---
+++ crowdin-translate/hi/src/translations/translations/locales/hi/commons/donationBox.ts
@@ -0,0 +1,8 @@
+const donationBox = {
+  LABEL_DONATE_FOR: 'Donate for',
+  LABEL_DONATE_BACK: 'Back',
+  LABEL_DONATE_MINIMUM: 'Minimum donation is',
+  LABEL_DONATE: 'Donate',
+  LABEL_EPICO_DESCRIPTION: 'Epico doesn’t take a fee!'
+};
+export default donationBox;
