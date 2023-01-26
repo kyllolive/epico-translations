be8d2ccfd0e9a02b9f61498f3c00cf94a590a41f
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 14:22:34 +0800

    New translations seriesDrawer.ts (Cebuano)

---
+++ crowdin-translate/ceb/src/translations/translations/locales/ko/commons/seriesDrawer.ts
@@ -0,0 +1,15 @@
+const seriesDrawer = {
+  LABEL_INFORMATION_TAB_NEW_SERIES_MSG: '새로운 챕터 매주 월요일, 금요일',
+  LABEL_INFORMATION: '정보',
+  LABEL_LANGUAGE: '언어',
+  LABEL_READ_CHAPTER_1: '읽기: 1장',
+  LABEL_CREATORS: '크리에이터',
+  LABEL_PUBLISHER: '발행자',
+  LABEL_ARTIST: '아티스트',
+  LABEL_AUTHOR: '작가',
+  LABEL_SEE_MORE: '더보기',
+  LABEL_PUBLISHER_OTHER_WORK: '출판사의 다른 작품',
+  LABEL_SERIES_YOU_MIGHT_LIKE: '좋아할 만한 시리즈',
+  LABEL_NOT_PUBLISHED_CHAPTER: '게시되지 않음'
+};
+export default seriesDrawer;
