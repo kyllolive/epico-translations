3a82d6d760f63dd52c76c359aa8dc9ade25e4d87
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 11:40:51 +0800

    New translations deleteDialog.ts (Spanish)

---
+++ crowdin-translate/es/test-dir/translations/locales/mr/commons/deleteDialog.ts
@@ -0,0 +1,11 @@
+const deleteDialog = {
+  LABEL_WARNING_CONFIRMATION: 'पुष्टीकरण आवश्यक आहे',
+  LABEL_WARNING_REMOVE: 'तुम्ही काढणार आहात',
+  LABEL_WARNING_REMOVE_MSG: 'हे पुनर्संचयित केले जाऊ शकत नाही! तुम्हाला खात्री आहे का?',
+  LABEL_DELETE_REMINDER: 'ही क्रिया डेटा गमावू शकते. अपघाती कृती टाळण्यासाठी आम्ही तुम्हाला तुमच्या हेतूची पुष्टी करण्यास सांगतो. कृपया टाइप करा',
+  LABEL_PROCEED_CANCEL_MSG: 'रद्द करण्यासाठी हे मॉडेल पुढे जाण्यासाठी किंवा बंद करण्यासाठी.',
+  LABEL_DELETE_TITLE: 'शीर्षक',
+  LABEL_DELETE_CANCEL: 'रद्द करा',
+  LABEL_DELETE_CONFIRM: 'पुष्टी'
+};
+export default deleteDialog;
