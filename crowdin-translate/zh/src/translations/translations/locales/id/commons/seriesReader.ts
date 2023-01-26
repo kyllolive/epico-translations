08faecbe54c8465ecec8c445a7c8a8105449dc46
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 14:01:37 +0800

    New translations seriesReader.ts (Chinese Simplified)

---
+++ crowdin-translate/zh/src/translations/translations/locales/id/commons/seriesReader.ts
@@ -0,0 +1,6 @@
+const seriesReader = {
+  LABEL_LOADING_CHAPTER: 'Loading chapter...',
+  LABEL_NO_CHAPTER_SELECTED: 'No chapter selected',
+  LABEL_NO_PERMISSION_TO_VIEW: 'No permission to view'
+};
+export default seriesReader;
