91a7f2e25b9eec01f62d2d3706df1b8d7f891f5c
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 13:48:01 +0800

    New translations imageUploader.ts (Spanish)

---
+++ crowdin-translate/es/src/translations/translations/locales/fil/commons/imageUploader.ts
@@ -0,0 +1,24 @@
+const imageUploader = {
+  LABEL_UPLOADING_WRONG_RESO: 'Oops! Mukhang maling resolution ang ina-upload mo',
+  LABEL_UPLOADING_MULTI_FILES: 'Oops! Mukhang nag-a-upload ka ng maraming file',
+  LABEL_UPLOADING_NOT_SUPPORTED_FILES: 'Oops! Mukhang nag-a-upload ka ng hindi sinusuportahang uri ng file',
+  LABEL_UPLOADING_MAX_FILES: 'Oops! Mukhang nag-a-upload ka ng mas mataas sa maximum na laki ng file',
+  LABEL_UPLOADING_MIN_FILES: 'Oops! Mukhang mas mababa sa minimum na laki ng file ang ina-upload mo',
+  LABEL_UPLOADING_CORRUPTED_FILES: 'Oops! Mukhang nag-a-upload ka ng sirang file',
+  LABEL_UPLOADING_REACH_MAX_SIZE: 'Oops! Mukhang naabot mo na ang kabuuang maximum na laki ng file',
+  LABEL_UPLOADING_REACH_MAX_FILE_COUNT: 'Oops! Parang naabot mo na ang max no. ng mga file na pinapayagan',
+  LABEL_CORRUPTED: 'Sirang file object',
+  LABEL_FILE: 'FILE',
+  LABEL_SIZE: 'SIZE',
+  LABEL_TOTAL: 'KABUUAN',
+  LABEL_NAME: 'PANGALAN',
+  LABEL_TYPE: 'URI',
+  LABEL_PAGE: 'PAGE',
+  LABEL_PAGES: 'PAGE',
+  LABEL_DROP_IMAGE_HERE: 'I-drop ang iyong mga larawan dito',
+  LABEL_OPTIMAL_PAGES_DIMENSION: 'Para sa pinakamahusay na mga resulta sa lahat ng mga device, ang bawat pahina ay hindi dapat higit sa 16000 x 16000 pixels',
+  LABEL_DROP_IT_HERE: 'Ihulog mo dito!',
+  LABEL_LOADING_IMG: 'Naglo-load ng mga larawan...',
+  LABEL_MAX: 'MAX'
+};
+export default imageUploader;
