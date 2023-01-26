14ae8fe08416339f3eceb307dbc4aafb88c8b225
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 12:09:35 +0800

    New translations genre.ts (Cebuano)

---
+++ crowdin-translate/ceb/test-dir/translations/locales/fil/genre.ts
@@ -0,0 +1,12 @@
+import genres from './commons/genres';
+import topNav from './commons/topNav';
+import common from './commons/common';
+import authModal from './commons/authModal';
+const genre = { ...genres,
+  ...topNav,
+  ...common,
+  ...authModal,
+  LABEL_LOADING_SERIES: 'Naglo-load ng serye...',
+  LABEL_FETCHING_MORE_SERIES: 'Kumukuha ng higit pang serye...'
+};
+export default genre;
