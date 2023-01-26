cc7532282b8d0fe930f591dc2275154af2a57a37
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 11:55:48 +0800

    New translations deleteDialog.ts (Indonesian)

---
+++ crowdin-translate/id/test-dir/translations/locales/ko/commons/deleteDialog.ts
@@ -0,0 +1,11 @@
+const deleteDialog = {
+  LABEL_WARNING_CONFIRMATION: '확인 필요',
+  LABEL_WARNING_REMOVE: '제거할 예정입니다.',
+  LABEL_WARNING_REMOVE_MSG: '이것은 복원할 수 없습니다! 당신은 절대적으로 확신합니까?',
+  LABEL_DELETE_REMINDER: '이 작업은 데이터 손실로 이어질 수 있습니다. 우발적인 행동을 방지하기 위해 귀하의 의도를 확인하시기 바랍니다. 입력하십시오',
+  LABEL_PROCEED_CANCEL_MSG: '진행하려면 이 모달을 닫거나 취소하려면 이 모달을 닫습니다.',
+  LABEL_DELETE_TITLE: '제목',
+  LABEL_DELETE_CANCEL: '취소',
+  LABEL_DELETE_CONFIRM: '확인하다'
+};
+export default deleteDialog;
