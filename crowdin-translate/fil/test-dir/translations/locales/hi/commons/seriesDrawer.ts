a94dd902a4056374ed3bfde49296b90216c933a2
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 12:06:23 +0800

    New translations seriesDrawer.ts (Filipino)

---
+++ crowdin-translate/fil/test-dir/translations/locales/hi/commons/seriesDrawer.ts
@@ -0,0 +1,15 @@
+const seriesDrawer = {
+  LABEL_INFORMATION_TAB_NEW_SERIES_MSG: 'नया अध्याय हर सोमवार, शुक्रवार',
+  LABEL_INFORMATION: 'जानकारी',
+  LABEL_LANGUAGE: 'भाषा',
+  LABEL_READ_CHAPTER_1: 'पढ़ें: अध्याय 1',
+  LABEL_CREATORS: 'रचनाकारों',
+  LABEL_PUBLISHER: 'प्रकाशक',
+  LABEL_ARTIST: 'कलाकार',
+  LABEL_AUTHOR: 'लेखक',
+  LABEL_SEE_MORE: 'और देखें',
+  LABEL_PUBLISHER_OTHER_WORK: 'प्रकाशक के अन्य कार्य',
+  LABEL_SERIES_YOU_MIGHT_LIKE: 'श्रृंखला आपको पसंद आ सकती है',
+  LABEL_NOT_PUBLISHED_CHAPTER: 'प्रकाशित नहीं हुआ'
+};
+export default seriesDrawer;
