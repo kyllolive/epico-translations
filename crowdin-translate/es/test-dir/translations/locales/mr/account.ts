9955c5c71f1a2d3211e95a94975d9a28b02fd9a4
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 11:40:48 +0800

    New translations account.ts (Spanish)

---
+++ crowdin-translate/es/test-dir/translations/locales/mr/account.ts
@@ -0,0 +1,11 @@
+import topNav from './commons/topNav';
+import common from './commons/common';
+import authModal from './commons/authModal';
+const account = { ...topNav,
+  ...common,
+  ...authModal,
+  LABEL_SIGN_IN: 'साइन इन करा',
+  LABEL_SIGN_OUT: 'साइन आउट करा',
+  LABEL_SETTINGS: 'सेटिंग्ज'
+};
+export default account;
