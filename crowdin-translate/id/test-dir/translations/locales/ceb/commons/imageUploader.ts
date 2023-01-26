4d1f158c0852406716c83185015ec10f6a74cc0a
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 11:52:37 +0800

    New translations imageUploader.ts (Indonesian)

---
+++ crowdin-translate/id/test-dir/translations/locales/ceb/commons/imageUploader.ts
@@ -0,0 +1,24 @@
+const imageUploader = {
+  LABEL_UPLOADING_WRONG_RESO: 'Oops! Morag sayop ang imong resolusyon nga nag-upload',
+  LABEL_UPLOADING_MULTI_FILES: 'Oops! Morag nag-upload ka og daghang mga file',
+  LABEL_UPLOADING_NOT_SUPPORTED_FILES: 'Oops! Morag nag-upload ka og dili suportadong tipo sa file',
+  LABEL_UPLOADING_MAX_FILES: 'Oops! Morag nag-upload ka nga mas taas kaysa labing taas nga gidak-on sa file',
+  LABEL_UPLOADING_MIN_FILES: 'Oops! Morag mas ubos ang imong gi-upload kaysa minimum nga gidak-on sa file',
+  LABEL_UPLOADING_CORRUPTED_FILES: 'Oops! Morag nag-upload ka og dunot nga file',
+  LABEL_UPLOADING_REACH_MAX_SIZE: 'Oops! Morag naabot na nimo ang kinatibuk-ang maximum nga gidak-on sa file',
+  LABEL_UPLOADING_REACH_MAX_FILE_COUNT: 'Oops! Murag naabot na nimo ang max no. sa mga file nga gitugotan',
+  LABEL_CORRUPTED: 'Nadaot nga file object',
+  LABEL_FILE: 'FILE',
+  LABEL_SIZE: 'SIZE',
+  LABEL_TOTAL: 'TOTAL',
+  LABEL_NAME: 'NGALAN',
+  LABEL_TYPE: 'MATANG',
+  LABEL_PAGE: 'PANID',
+  LABEL_PAGES: 'MGA PANID',
+  LABEL_DROP_IMAGE_HERE: 'Ihulog ang imong mga hulagway dinhi',
+  LABEL_OPTIMAL_PAGES_DIMENSION: 'Para sa labing maayong resulta sa tanang device, ang matag panid kinahanglang dili molapas sa 16000 x 16000 pixels',
+  LABEL_DROP_IT_HERE: 'Ihulog kini dinhi!',
+  LABEL_LOADING_IMG: 'Nagkarga og mga hulagway...',
+  LABEL_MAX: 'MAX'
+};
+export default imageUploader;
