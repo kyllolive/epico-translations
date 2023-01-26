550f7b09a5e45148f0cc70afa1b0075ef902da3c
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 12:03:07 +0800

    New translations index.ts (Hindi)

---
+++ crowdin-translate/hi/test-dir/translations/locales/index.ts
@@ -0,0 +1,24 @@
+import ceb from './ceb';
+import en from './en';
+import es from './es';
+import fil from './fil';
+import fr from './fr';
+import hi from './hi';
+import id from './id';
+import ja from './ja';
+import ko from './ko';
+import mr from './mr';
+import zh from './zh';
+export default {
+  ceb,
+  en,
+  es,
+  fil,
+  fr,
+  hi,
+  id,
+  ja,
+  ko,
+  mr,
+  zh
+};
