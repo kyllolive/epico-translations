a5f5d35d8c56b2b14787fcd900f683a67b097ddd
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 11:55:25 +0800

    New translations donationBox.ts (Indonesian)

---
+++ crowdin-translate/id/test-dir/translations/locales/ja/commons/donationBox.ts
@@ -0,0 +1,8 @@
+const donationBox = {
+  LABEL_DONATE_FOR: '寄付する',
+  LABEL_DONATE_BACK: '戻る',
+  LABEL_DONATE_MINIMUM: '最低限の寄付は',
+  LABEL_DONATE: '寄付',
+  LABEL_EPICO_DESCRIPTION: 'エピコは手数料がかかりません！'
+};
+export default donationBox;
