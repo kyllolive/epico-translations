17ce0bd691ec60235ba0cd06ba2b7181754b43b3
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 14:21:57 +0800

    New translations viewChapter.ts (Cebuano)

---
+++ crowdin-translate/ceb/src/translations/translations/locales/id/viewChapter.ts
@@ -0,0 +1,19 @@
+import genres from './commons/genres';
+import common from './commons/common';
+import seriesDrawer from './commons/seriesDrawer';
+import seriesReader from './commons/seriesReader';
+import seriesFooter from './commons/seriesFooter';
+import authModal from './commons/authModal';
+import followButton from './commons/followButton';
+const viewChapter = { ...genres,
+  ...common,
+  ...seriesDrawer,
+  ...seriesReader,
+  ...seriesFooter,
+  ...authModal,
+  ...followButton,
+  LABEL_WARNING_MESSAGE: `This Series contains adult themes and situations and is recommended
+  for mature audiences. Viewer's discretions is advised. Proceed to view
+  content?`
+};
+export default viewChapter;
