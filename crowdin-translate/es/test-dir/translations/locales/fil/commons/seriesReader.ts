200c271beefc269321bfb0374e179a7f4664aaa2
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 11:38:30 +0800

    New translations seriesReader.ts (Spanish)

---
+++ crowdin-translate/es/test-dir/translations/locales/fil/commons/seriesReader.ts
@@ -0,0 +1,6 @@
+const seriesReader = {
+  LABEL_LOADING_CHAPTER: 'Nilo-load ang kabanata...',
+  LABEL_NO_CHAPTER_SELECTED: 'Walang napiling kabanata',
+  LABEL_NO_PERMISSION_TO_VIEW: 'Walang pahintulot na tingnan'
+};
+export default seriesReader;
