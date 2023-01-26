678faef34050f019856df64a13e864777d058b33
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 13:52:06 +0800

    New translations topNav.ts (Japanese)

---
+++ crowdin-translate/ja/src/translations/translations/locales/fil/commons/topNav.ts
@@ -0,0 +1,20 @@
+const topNav = {
+  LABEL_HOME: 'Bahay',
+  LABEL_GENRE: 'Mga genre',
+  LABEL_TRANSLATE: 'Scanlate',
+  LABEL_NOTIFICATION: 'Mga abiso',
+  LABEL_ACCOUNT: 'Aking Account',
+  LABEL_ABOUT_US: 'Tungkol sa atin',
+  LABEL_SIGN_IN: 'Mag-sign in',
+  LABEL_SIGN_OUT: 'Mag-sign out',
+  LABEL_SETTINGS: 'Mga setting',
+  LABEL_SEARCH: 'Maghanap',
+  LABEL_PUBLISH: 'I-publish',
+  TOOLTIP_CREATE_SERIES: 'Gumawa ng serye',
+  TOOLTIP_SERIES_LIST: 'Listahan ng serye',
+  TOOLTIP_MENU: 'Menu',
+  LABEL_SIGNED_IN_AS: 'Naka-sign in bilang',
+  LABEL_MY_PROFILE: 'Aking Profile',
+  LABEL_MY_ACCOUNT_SETTINGS: 'Setting ng Aking Account'
+};
+export default topNav;
