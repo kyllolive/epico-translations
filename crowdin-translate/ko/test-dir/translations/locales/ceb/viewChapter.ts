01aec6400ed3aa3a3512f054ba8ff4dc79a65737
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 11:45:11 +0800

    New translations viewChapter.ts (Korean)

---
+++ crowdin-translate/ko/test-dir/translations/locales/ceb/viewChapter.ts
@@ -0,0 +1,19 @@
+import genres from './commons/genres';
+import common from './commons/common';
+import seriesDrawer from './commons/seriesDrawer';
+import seriesReader from './commons/seriesReader';
+import seriesFooter from './commons/seriesFooter';
+import authModal from './commons/authModal';
+import followButton from './commons/followButton';
+const viewChapter = { ...genres,
+  ...common,
+  ...seriesDrawer,
+  ...seriesReader,
+  ...seriesFooter,
+  ...authModal,
+  ...followButton,
+  LABEL_WARNING_MESSAGE: `Kini nga Serye adunay mga tema ug sitwasyon sa mga hamtong ug girekomenda
+  alang sa hamtong nga mga tumatan-aw. Gitambagan ang mga kabuot sa tumatan-aw. Padayon sa pagtan-aw
+  sulod?`
+};
+export default viewChapter;
