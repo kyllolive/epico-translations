ae33cc4ae543a938b6f7d3b090c74e7f6eb2d878
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 14:22:30 +0800

    New translations donationBox.ts (Cebuano)

---
+++ crowdin-translate/ceb/src/translations/translations/locales/ko/commons/donationBox.ts
@@ -0,0 +1,8 @@
+const donationBox = {
+  LABEL_DONATE_FOR: '기부',
+  LABEL_DONATE_BACK: '뒤',
+  LABEL_DONATE_MINIMUM: '최소 기부금은',
+  LABEL_DONATE: '기부',
+  LABEL_EPICO_DESCRIPTION: '에피코는 수수료를 받지 않습니다!'
+};
+export default donationBox;
