a82ba1a53871118fda88ed6068f19ea4d3e42201
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 12:02:20 +0800

    New translations deleteDialog.ts (Hindi)

---
+++ crowdin-translate/hi/test-dir/translations/locales/hi/commons/deleteDialog.ts
@@ -0,0 +1,11 @@
+const deleteDialog = {
+  LABEL_WARNING_CONFIRMATION: 'पुष्टिकरण की आवश्यकता है',
+  LABEL_WARNING_REMOVE: 'आप हटाने जा रहे हैं',
+  LABEL_WARNING_REMOVE_MSG: 'इसे बहाल नहीं किया जा सकता है! क्या आप बिल्कुल निश्चित हैं?',
+  LABEL_DELETE_REMINDER: 'इस कार्रवाई से डेटा हानि हो सकती है। आकस्मिक कार्यों को रोकने के लिए हम आपसे अपने इरादे की पुष्टि करने के लिए कहते हैं। कृप्या टाइप करे',
+  LABEL_PROCEED_CANCEL_MSG: 'रद्द करने के लिए इस मोडल को आगे बढ़ाने या बंद करने के लिए।',
+  LABEL_DELETE_TITLE: 'शीर्षक',
+  LABEL_DELETE_CANCEL: 'रद्द करना',
+  LABEL_DELETE_CONFIRM: 'पुष्टि करें'
+};
+export default deleteDialog;
