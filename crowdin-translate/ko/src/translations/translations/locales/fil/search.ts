7255f6893fb1813854d5b9289673b0086e596ffb
kyllolive <75357161+kyllolive@users.noreply.github.com>
2023-01-26 13:56:22 +0800

    New translations search.ts (Korean)

---
+++ crowdin-translate/ko/src/translations/translations/locales/fil/search.ts
@@ -0,0 +1,13 @@
+import topNav from './commons/topNav';
+import common from './commons/common';
+import authModal from './commons/authModal';
+import genres from './commons/genres';
+const search = { ...topNav,
+  ...common,
+  ...authModal,
+  ...genres,
+  LABEL_SEARCH_PLACEHOLDER: 'Maghanap ng mga serye at tagalikha',
+  LABEL_MORE_FETCHING_CONTENTS: 'Kinukuha ang higit pang nilalaman...',
+  LABEL_MORE_SEARCHING_CONTENTS: 'Naghahanap ng mga nilalaman...'
+};
+export default search;
